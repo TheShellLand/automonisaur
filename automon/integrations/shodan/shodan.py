dd71462d0e83cac4d77390f315357934dc62ed65
ainiml <ericjaw@gmail.com>
2020-07-10 04:19:48 -0400

    add shodan

---
+++ automon/integrations/shodan/shodan.py
@@ -0,0 +1,11 @@
+class Shodan:
+    """Get any Shodan information
+    """
+
+    def __init__(self):
+        # TODO: move key to config
+        self.api_key = 'yKIZ2L54cQxIfnebD4V2qgPp0QQsJLpK'
+
+    # TODO: add shodan geoip
+    def request(self):
+        pass
