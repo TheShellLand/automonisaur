afb6bbc8837592be398d7291608efe0a874b3f6a
ainiml <ericjaw@gmail.com>
2020-07-10 04:19:55 -0400

    add geoip

---
+++ automon/integrations/geoip/geoip.py
@@ -0,0 +1,10 @@
+class Geoip:
+    """With the given IP address, retrieve geoip information
+    """
+
+    def __init__(self, ip):
+        self.ip = ip
+
+    # TODO: parse geoiptool.com
+    def geoiptool(self):
+        pass
