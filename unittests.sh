fef257bf0dc573a8b0a2580c4c901d8ba7673fff
ainiml <ericjaw@gmail.com>
2020-07-11 19:01:15 -0400

    update unittests.sh

--- unittests.sh
+++ unittests.sh
@@ -9,6 +9,8 @@ if [ -z "$@" ]; then
   pytest --cov=automon -v --cov-report term automon
 elif [ "$@" == "less" ]; then
     pytest -v automon
+elif [ "$@" == "ll" ]; then
+    pytest automon
 elif [ "$@" == "html" ]; then
     pytest --cov=automon -v --cov-report html automon
 else
