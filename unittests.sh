a1fc52a7291c660ae955227be123e47f4184d34a
ainiml <ericjaw@gmail.com>
2020-07-11 01:24:15 -0400

    update unittests.sh

--- unittests.sh
+++ unittests.sh
@@ -6,9 +6,9 @@ cd $(dirname $0)
 # TODO: set pre-commit hook
 
 if [ -z "$@" ]; then
-  pytest --cov=automon --cov-report term automon
+  pytest --cov=automon -v --cov-report term automon
 elif [ "$@" == "html" ]; then
-    pytest --cov=automon --cov-report html automon
+    pytest --cov=automon -v --cov-report html automon
 else
-  pytest --cov=automon --cov-report term "$@"
+  pytest --cov=automon -v --cov-report term "$@"
 fi
