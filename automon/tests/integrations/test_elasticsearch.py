f4bbf36f59fd952d8c8188d7c7acd3a96455f4a1
ainiml <ericjaw@gmail.com>
2020-07-11 19:10:16 -0400

    refactor elasticsearch test

--- automon/tests/integrations/test_elasticsearch.py
+++ automon/tests/integrations/test_elasticsearch.py
@@ -1,7 +1,6 @@
 import unittest
 
-from automon.integrations.elasticsearch.config import JVMBot
-from automon.integrations.elasticsearch.config import SnapshotBot
+from automon.integrations.elasticsearch.config import ElasticsearchConfig, SnapshotBot, JVMBot
 from automon.integrations.elasticsearch.cleanup import Cleanup
 
 
@@ -12,10 +11,13 @@ class ElasticsearchTest(unittest.TestCase):
             Cleanup(
                 'elasticsearch.0000000', use_ssl=False, request_timeout=1).elasticsearch.ping())
 
-    def test_ConfigESSnapshotBot(self):
+    def test_ElasticsearchConfig(self):
+        self.assertTrue(ElasticsearchConfig())
+
+    def test_SnapshotBot(self):
         self.assertTrue(SnapshotBot())
 
-    def test_ConfigESJVMBot(self):
+    def test_JVMBot(self):
         self.assertTrue(JVMBot())
 
     def test_ElasticsearchConnect(self):
