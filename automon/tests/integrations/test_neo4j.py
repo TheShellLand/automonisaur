242b82f20ad3c647ab789f3f8dd91f0bc8dff2a2
ainiml <ericjaw@gmail.com>
2020-07-11 01:06:28 -0400

    add neo4j test

---
+++ automon/tests/integrations/test_neo4j.py
@@ -0,0 +1,14 @@
+import json
+import unittest
+
+from automon.integrations.neo4j import Neo4jWrapper
+
+
+class Neo4jTest(unittest.TestCase):
+
+    def test_Neo4jWrapper(self):
+        self.assertTrue(Neo4jWrapper())
+
+
+if __name__ == '__main__':
+    unittest.main()
