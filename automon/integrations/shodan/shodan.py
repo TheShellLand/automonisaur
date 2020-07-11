0c84538fbcce1a3736abf09f8d032c46b4b8c551
ainiml <ericjaw@gmail.com>
2020-07-11 01:01:32 -0400

    add shodan tests

--- automon/integrations/shodan/shodan.py
+++ automon/integrations/shodan/shodan.py
@@ -1,10 +1,25 @@
-class Shodan:
-    """Get any Shodan information
-    """
+import os
+
+from automon.logger import Logging
+
+log = Logging(__name__, Logging.ERROR)
+
+
+class ShodanConfig:
+    token = os.getenv('SHODAN_API')
 
     def __init__(self):
-        # TODO: move key to config
-        self.api_key = 'yKIZ2L54cQxIfnebD4V2qgPp0QQsJLpK'
+        self.token = os.getenv('SHODAN_API')
+
+        if not self.token:
+            log.error(f'Missing Shodan api key')
+
+
+class Shodan:
+    """Get any Shodan information"""
+
+    def __init__(self, api_key: str = None):
+        self.api_key = api_key if api_key else ShodanConfig.token
 
     # TODO: add shodan geoip
     def request(self):
