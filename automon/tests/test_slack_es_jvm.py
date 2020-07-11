6b6a37fc5dfa03b628aa1610733e439672802bfc
ainiml <ericjaw@gmail.com>
2020-07-11 01:01:44 -0400

    delete old tests

--- automon/tests/test_slack_es_jvm.py
+++
@@ -1,7 +0,0 @@
-from automon.integrations.slack.slack import Slack
-from automon.integrations.elasticsearch.config import ConfigESJVMBot
-
-
-def test_es_jvm_bot():
-    conf = ConfigESJVMBot()
-    Slack(username=conf.slack_name).alert('This is a JVM test')
