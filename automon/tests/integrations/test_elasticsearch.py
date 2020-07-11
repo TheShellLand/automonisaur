d7175802c5bdc2d3f8e178c2465866c2980d3dfa
ainiml <ericjaw@gmail.com>
2020-07-11 01:06:34 -0400

    add elasticsearch test

---
+++ automon/tests/integrations/test_elasticsearch.py
@@ -0,0 +1,21 @@
+import unittest
+
+from automon.integrations.elasticsearch import ElasticsearchConnect
+from automon.integrations.elasticsearch.config import ConfigESJVMBot
+from automon.integrations.elasticsearch.config import ConfigESSnapshotBot
+
+
+class ElasticsearchTest(unittest.TestCase):
+
+    def test_ElasticsearchConnect(self):
+        self.assertFalse(
+            ElasticsearchConnect('elasticsearch.0000000', use_ssl=False, request_timeout=1).eswrapper.ping())
+
+    def test_ConfigESSnapshotBot(self):
+        self.assertTrue(ConfigESSnapshotBot())
+
+    def test_ConfigESJVMBot(self):
+        self.assertTrue(ConfigESJVMBot())
+
+# if __name__ == '__main__':
+#     unittest.main()
