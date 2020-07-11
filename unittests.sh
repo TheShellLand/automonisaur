05dd17033b9601e06c2f0fa2080e9a47e2a2fdab
ainiml <ericjaw@gmail.com>
2020-07-11 18:17:24 -0400

    added -v test flag

--- unittests.sh
+++ unittests.sh
@@ -8,7 +8,7 @@ cd $(dirname $0)
 if [ -z "$@" ]; then
   pytest --cov=automon -v --cov-report term automon
 elif [ "$@" == "less" ]; then
-    pytest automon
+    pytest -v automon
 elif [ "$@" == "html" ]; then
     pytest --cov=automon -v --cov-report html automon
 else
