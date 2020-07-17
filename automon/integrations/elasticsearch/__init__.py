a06862c3187a8cc99f790e1ce0897fd0d3b51757
ainiml <naisanza@gmail.com>
2020-07-17 17:46:27 -0400

    refactored elasticsearch

--- automon/integrations/elasticsearch/__init__.py
+++ automon/integrations/elasticsearch/__init__.py
@@ -1,6 +1,5 @@
 from .jvm import *
-from .metric import *
-from .cluster import *
+from .metrics import *
 from .cleanup import *
 from .snapshots import *
 from .monitoring import *
--- automon/integrations/elasticsearch/metrics.py
+++ automon/integrations/elasticsearch/metrics.py
@@ -1,6 +1,26 @@
 import datetime
 
 
+class Cluster:
+    def __init__(self, raw_metrics):
+        self._all_metrics = raw_metrics
+        self._nodes = {
+            'total': self._all_metrics['_nodes']['total'],
+            'successful': self._all_metrics['_nodes']['successful'],
+            'failed': self._all_metrics['_nodes']['failed']
+        }
+
+        self.cluster_name = self._all_metrics['cluster_name']
+        self.nodes = []
+        self.metrics = []
+
+        for node in self._all_metrics['nodes'].items():
+            self.nodes.append(node[1])
+
+        for metric in self.nodes:
+            self.metrics.append(Metric(metric))
+
+
 class Metric:
     """
 
@@ -75,7 +95,7 @@ class Metric:
 
     """
 
-    def __init__(self, node):
+    def __init__(self, node: Cluster.nodes):
         self._node = node
         self.node_name = self._node['name']
 
