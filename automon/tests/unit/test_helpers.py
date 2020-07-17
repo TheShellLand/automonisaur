7a126fe38231aa0c6736ae79ec9421f1bc2576da
ainiml <naisanza@gmail.com>
2020-07-17 16:55:31 -0400

    add sanitation tests

--- automon/tests/unit/test_helpers.py
+++ automon/tests/unit/test_helpers.py
@@ -2,6 +2,49 @@ import unittest
 
 from automon.helpers.assertions import *
 from automon.helpers.sleeper import *
+from automon.helpers.sanitation import *
+
+
+class SanitationTest(unittest.TestCase):
+    def test_Sanitation(self):
+        self.assertTrue(Sanitation)
+        self.assertTrue(Sanitation.strip_quotes)
+        self.assertTrue(Sanitation.strip_spaces)
+        self.assertTrue(Sanitation.strip_spaces_from_list)
+        self.assertTrue(Sanitation.safe_string)
+        self.assertTrue(Sanitation.dedup)
+        self.assertTrue(Sanitation.list_from_string)
+
+        self.assertRaises(TypeError, Sanitation.strip_quotes)
+        self.assertRaises(TypeError, Sanitation.strip_spaces)
+        self.assertRaises(TypeError, Sanitation.strip_spaces_from_list)
+        self.assertRaises(TypeError, Sanitation.safe_string)
+        self.assertRaises(TypeError, Sanitation.dedup)
+        self.assertRaises(TypeError, Sanitation.list_from_string)
+
+        self.assertEqual(Sanitation.strip_quotes('test'), 'test')
+        self.assertEqual(Sanitation.strip_spaces(' t e s t '), 't e s t')
+        self.assertEqual(Sanitation.strip_spaces(' t e s t '), 't e s t')
+        self.assertEqual(Sanitation.strip_spaces_from_list(
+            [' t e s t ']), ['t e s t'])
+        self.assertEqual(Sanitation.safe_string(
+            'a;skdfjAS*&D)(&!H!:@JEN'), 'a_skdfjAS__D____H___JEN')
+        self.assertEqual(Sanitation.dedup(
+            [1, 1, 'AAAAA', 'AAAAA', 'AA', 555, 555, 5555, 6666, 7, 8, 33, 1, 1232, 499124, 'a']),
+            [1, 'AAAAA', 'AA', 555, 5555, 6666, 7, 8, 33, 1232, 499124, 'a'])
+
+        self.assertEqual(Sanitation.list_from_string(
+            'a,d,a,3,1,5,g,2,,a,4,1,,h,4,1,k,z'),
+            ['a', 'd', 'a', '3', '1', '5', 'g', '2', '', 'a', '4', '1', '', 'h', '4', '1', 'k', 'z']
+        )
+        self.assertEqual(Sanitation.list_from_string(
+            'a d a 3 1 5 g 2  a 4 1  h 4 1 k z'),
+            ['a', 'd', 'a', '3', '1', '5', 'g', '2', '', 'a', '4', '1', '', 'h', '4', '1', 'k', 'z']
+        )
+        self.assertEqual(Sanitation.list_from_string(
+            ' ada315g2a41h41kz '),
+            ['ada315g2a41h41kz']
+        )
 
 
 class SleeperTest(unittest.TestCase):
@@ -17,17 +60,16 @@ class SleeperTest(unittest.TestCase):
         self.assertTrue(Sleeper.daily)
         self.assertTrue(Sleeper.time_range)
 
-        with self.assertRaises(TypeError):
-            Sleeper.seconds()
-            Sleeper.seconds('1')
-            Sleeper.minute()
-            Sleeper.within_a_minute()
-            Sleeper.minutes()
-            Sleeper.hour()
-            Sleeper.hours()
-            Sleeper.day()
-            Sleeper.daily()
-            Sleeper.time_range()
+        self.assertRaises(TypeError, Sleeper.seconds)
+        self.assertRaises(TypeError, Sleeper.minute)
+        self.assertRaises(TypeError, Sleeper.within_a_minute)
+        self.assertRaises(TypeError, Sleeper.minutes)
+        self.assertRaises(TypeError, Sleeper.hour)
+        self.assertRaises(TypeError, Sleeper.hours)
+        self.assertRaises(TypeError, Sleeper.day)
+        self.assertRaises(TypeError, Sleeper.daily)
+        self.assertRaises(TypeError, Sleeper.time_range)
+        self.assertRaises(TypeError, Sleeper.seconds, '1')
 
         self.assertIsNone(Sleeper.seconds(SleeperTest, 0))
         self.assertIsNone(Sleeper.minute(SleeperTest, 0))
