20ddf6967904bf5f422c849ce9aa6990beb51dbc
ainiml <naisanza@gmail.com>
2020-07-18 12:37:30 -0400

    added flask tests

--- automon/tests/integrations/test_flask.py
+++ automon/tests/integrations/test_flask.py
@@ -1,18 +1,21 @@
 import unittest
 
+from flask import Flask
+
 from automon.integrations.flask import new_secret_key
 from automon.integrations.flask.config import javascript_compatibility
 from automon.integrations.flask.hashing import hash_key
 
 
 class FlaskTest(unittest.TestCase):
+
+    app = Flask(__name__)
+
     def test_new_secret_key(self):
         self.assertTrue(new_secret_key())
 
-    # TODO: add javascript_compatibility test
-    # def test_javascript_compatibility(self):
-    #     app = newflaskapp()
-    #     self.assertTrue(javascript_compatibility(app))
+    def test_javascript_compatibility(self):
+        self.assertTrue(javascript_compatibility(self.app))
 
     def test_hash_key(self):
         self.assertTrue(hash_key('blob'))
