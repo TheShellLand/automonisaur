d7779012e01493232623f35b9f53475f0836e1e7
ainiml <naisanza@gmail.com>
2020-07-18 12:47:45 -0400

    refactored shodan

--- automon/integrations/shodan/__init__.py
+++ automon/integrations/shodan/__init__.py
@@ -1,1 +1,26 @@
-from .shodan import *
+import os
+
+from automon.log.logger import Logging
+
+log = Logging(__name__, Logging.ERROR)
+
+
+class ShodanConfig:
+    token = os.getenv('SHODAN_API')
+
+    def __init__(self):
+        self.token = os.getenv('SHODAN_API')
+
+        if not self.token:
+            log.error(f'Missing SHODAN_API')
+
+
+class Shodan:
+    """Get any Shodan information"""
+
+    def __init__(self, api_key: str = None):
+        self.api_key = api_key if api_key else ShodanConfig.token
+
+    # TODO: add shodan geoip
+    def request(self):
+        pass
