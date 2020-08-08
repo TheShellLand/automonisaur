9154ec3f16e674f74b56869f6aa32abd7b889a4f
ainiml <naisanza@gmail.com>
2020-08-08 06:49:44 -0400

    updated phantom

--- automon/integrations/phantom/rules.py
+++ automon/integrations/phantom/rules.py
@@ -1,27 +1,29 @@
 import logging
 
-from automon.integrations.phantom.phantom_unittest import import_playbook
+from phantom.phantom_unittest import import_playbook, container, results, artifact
 
+timestamp = '%(asctime)s'
 levelname = '%(levelname)s'
 modname = '%(name)s'
 message = '%(message)s'
-log_format = f'{levelname}\t({modname})\t{message}'
+log_format = f'{timestamp}\t{levelname}\t({modname})\t{message}'
 
 logging.basicConfig(level=logging.DEBUG, format=log_format)
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-def playbook(playbook_to_import: str, container: str, name: str, callback: object):
+def playbook(playbook_to_import: str, container: dict = container(),
+             name: str = '', callback: object = None):
     """Mock function"""
     log = logging.getLogger(f'{__name__}.{playbook.__name__}')
 
-    log.info(f'playbook_to_import: str, container: str, name: str, callback: object')
+    # log.info(f'playbook_to_import: str, container: dict = {}, name: str, callback: object')
 
-    log.debug(playbook_to_import)
-    log.debug(container)
-    log.debug(name)
-    log.debug(callback)
+    log.debug(f'playbook_to_import: {playbook_to_import}')
+    log.debug(f'container: {container}')
+    log.debug(f'name: {name}')
+    log.debug(f'callback: {callback}')
 
     return import_playbook(playbook_to_import)
 
@@ -30,36 +32,42 @@ def get_run_data(key: str, **kwargs):
     """Mock function"""
     log = logging.getLogger(f'{__name__}.{get_run_data.__name__}')
 
-    log.info(f'key')
-
-    log.debug(key)
+    # log.info(f'key: {key}')
+    log.debug(f'key: {key}')
 
     return key
 
 
-def condition(container: str, conditions: list, name: str):
+def condition(container: dict = container(), conditions: list = [], name: str = ''):
     """Mock function"""
     log = logging.getLogger(f'{__name__}.{condition.__name__}')
 
-    log.debug(f'container: str, conditions: list, name: str')
+    log.debug(f'{help(condition)}')
 
-    log.debug(container)
-    log.debug(conditions)
-    log.debug(name)
+    log.debug(f'container: {container}')
+    log.debug(f'conditions: {conditions}')
+    log.debug(f'name: {name}')
 
     return container, conditions, name
 
 
-def format(container: object = None, template: str = None, parameters: list = None, name: str = None):
+def format(container: dict = container(), template: str = '', parameters: list = [str],
+           name: str = ''):
     """Mock function"""
     log = logging.getLogger(f'{__name__}.{format.__name__}')
 
-    log.debug(f'container: object, template: str, parameters: list, name: str')
+    log.debug('container: dict = {}, template: str = '', parameters: list = [], name: str = ''')
 
-    log.debug(f'{container}')
-    log.debug(f'{template}')
-    log.debug(f'{parameters}')
-    log.debug(f'{name}')
+    parameters_orig = parameters
+
+    parameters = [
+        [x.split(':')] for x in parameters
+    ]
+
+    log.debug(f'container: {container}')
+    log.debug(f'template: {template}')
+    log.debug(f'parameters: {parameters}')
+    log.debug(f'name: {name}')
 
     return container, template, parameters, name
 
@@ -73,7 +81,20 @@ def get_format_data(name: str, **kwargs):
     return name
 
 
-def act(action: str, parameters: str, assets: list, callback: object, name: str):
+def collect2(container: dict = container(), datapath: list = [], action_results: object = None):
+    """Mock function"""
+    log = logging.getLogger(f'{__name__}.{collect2.__name__}')
+
+    log.info('container: dict = {}, datapath: list = [], action_results: object = None')
+
+    log.debug(f'container: {container}')
+    log.debug(f'datapath: {datapath}')
+    log.debug(f'action_results: {action_results}')
+
+    return [[artifact(), artifact()]]
+
+
+def act(action: str, parameters: str, assets: list, callback: object, name: str, parent_action: str = ''):
     """Mock function"""
     log = logging.getLogger(f'{__name__}.{act.__name__}')
 
@@ -84,8 +105,9 @@ def act(action: str, parameters: str, assets: list, callback: object, name: str)
     log.debug(f'assets: {assets}')
     log.debug(f'callback: {callback}')
     log.debug(f'name: {name}')
+    log.debug(f'parent_action: {parent_action}')
 
-    return action, parameters, assets, callback, name
+    return action, parameters, assets, callback, name, parent_action
 
 
 def save_run_data(key: str, value: str, auto: bool):
@@ -100,8 +122,8 @@ def save_run_data(key: str, value: str, auto: bool):
 
 
 def debug(object: str):
-    return log.info(f'{object}')
+    return log.debug(f'{object}')
 
 
 def error(object: str):
-    return log.info(f'{object}')
+    return log.error(f'{object}')
