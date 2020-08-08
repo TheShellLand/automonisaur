f4fb58a9d5001c5ccccf4af2aea1461c44697c13
ainiml <naisanza@gmail.com>
2020-08-08 06:49:44 -0400

    updated phantom

--- automon/integrations/phantom/phantom_unittest.py
+++ automon/integrations/phantom/phantom_unittest.py
@@ -28,3 +28,33 @@ def import_playbook(playbook: str):
     if 'master/' in playbook:
         playbook = playbook.replace('master/', '')
     return importlib.import_module(playbook)
+
+
+def container():
+    return {
+        'id': 0
+    }
+
+
+def results():
+    return [
+        'cef_dictionary',
+        'artifact_id'
+    ]
+
+
+def artifact(id_value: int = 0, source_data_identifier_value: str = '', results_item_1: list = results()):
+    return {
+            'container_id': id_value,
+            'name': "Marriott User Validation",
+            'contains': "{}",
+            'source_data_identifier': source_data_identifier_value,
+            'label': "enrich",
+            'cef_value': "",
+            'dedup_is_success': True,
+            'run_automation': False,
+            'cef_name': "",
+            'cef_dictionary': results_item_1[0],
+            # context (artifact id) is added to associate results with the artifact
+            'context': {'artifact_id': results_item_1[1]},
+        }
