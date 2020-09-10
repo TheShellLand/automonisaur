dded61e1af1a4046401b6d0cdf06eb73434dba5b
ainiml <ericjaw@gmail.com>
2020-09-10 16:28:22 -0400

    added basic splunk integration

--- automon/integrations/splunk/client.py
+++ automon/integrations/splunk/client.py
@@ -1,20 +1,32 @@
 import splunklib.client as client
 
+from automon.integrations.splunk.config import SplunkConfig
 
-class SplunkClient(client):
-    def __init__(self, host: str = 'localhost', port: int = 8089,
-                 username: str = 'admin', password: str = ''):
-        self.host = host
-        self.port = port
-        self.username = username
 
-        self.service = client.connect(
-            host=host, port=port,
-            username=username, password=password)
+class SplunkClient:
+    def __init__(self):
+        self.config = SplunkConfig()
+        self.client = client.connect(
+            host=self.config.host,
+            port=self.config.port,
+            username=self.config.username,
+            password=self.config.password)
 
-        self.apps = self.service.apps
-
+        # referred to as a service in docs
+        self.service = self.client
         assert isinstance(self.service, client.Service)
+
+        self.apps = self.client.apps
+
+    def get_client(self):
+        return self.client
+
+    def get_service(self):
+        return self.client.connect()
+
+    def get_apps(self):
+        return [x for x in self.service.apps]
+
 
     def create_app(self, app_name):
         return self.apps.create(app_name)
@@ -27,3 +39,7 @@ class SplunkClient(client):
 
     def app_info(self, app_name):
         return f'{self.apps[app_name]}'
+
+
+    def __str__(self):
+        return f'{self.client}'
