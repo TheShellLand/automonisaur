71e1ca9863b57f3150b62f099a3b6fd7c262cf4c
naisanzaa <naisanza@gmail.com>
2021-02-01 11:33:23 -0500

    bump v0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
