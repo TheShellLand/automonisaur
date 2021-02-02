2f6075847361f1d7b3c229dc604bde749e9df7c2
naisanzaa <naisanza@gmail.com>
2021-02-02 14:04:15 -0500

    fix SlackError
    - fix error handling
    - add error handling for error 'reason'

--- automon/integrations/slack/slack.py
+++ automon/integrations/slack/slack.py
@@ -28,23 +28,50 @@ class SlackError:
         """
 
         self._error = error
-        self._response = self._error.response
+        self._reason = getattr(self._error, 'reason', '')
+        self._response = getattr(self._error, 'response', '')
 
-        self.api_url = self._response.api_url
+        if self._reason:
+            self.args = getattr(self._reason, 'args', '')
+            self.errno = getattr(self._reason, 'errno', '')
+            self.reason = getattr(self._reason, 'reason', '')
+            self.strerror = getattr(self._reason, 'strerror', '')
+            self.verfify_code = getattr(self._reason, 'verify_code', '')
+            self.verify_message = getattr(self._reason, 'verify_message', '')
 
-        self.data = dict(self._response.data)
-        self.ok = self.data.get('ok')
-        self.error = self.data.get('error')
-        self.needed = self.data.get('needed')
-        self.provided = self.data.get('provided')
+        if self._response:
+            self.api_url = getattr(self._response, 'api_url' '')
 
-        self.headers = self._response.headers
-        self.http_verb = self._response.http_verb
-        self.req_args = self._response.req_args
-        self.status_code = self._response.status_code
+            self.data = dict(getattr(self._response, 'data', ''))
+            self.ok = self.data.get('ok', '')
+            self.error = self.data.get('error', '')
+            self._needed = self.data.get('needed', '')
+            self.provided = self.data.get('provided', '')
+
+            self.headers = getattr(self._response, 'headers', '')
+            self.http_verb = getattr(self._response, 'http_verb', '')
+            self.req_args = getattr(self._response, 'req_args', '')
+            self.status_code = getattr(self._response, 'status_code', '')
+
+    def needed(self):
+        if self._response:
+            return self._needed
+
+        if self._reason:
+            return self.strerror
+
+        log.info(f'{NotImplemented}')
+        return f'{self._error}'
 
     def __repr__(self):
-        return f'{self.data}'
+        if self._response:
+            return f'{self.data}'
+
+        if self._reason:
+            return f'{self.strerror}'
+
+        log.info(f'{NotImplemented}')
+        return f'{self._error}'
 
     def __str__(self):
         return self.__repr__()
