6fa708764ec855f727fd56211e6ac53b9e5766c9
naisanza <naisanza@gmail.com>
2021-02-22 01:33:51 -0500

    fix ElasticsearchJvmMonitor connection check

--- automon/integrations/elasticsearch/jvm.py
+++ automon/integrations/elasticsearch/jvm.py
@@ -11,24 +11,29 @@ class ElasticsearchJvmMonitor:
         self._log = Logging(ElasticsearchJvmMonitor.__name__, Logging.DEBUG)
 
         self._config = config if isinstance(config, ElasticsearchConfig) else ElasticsearchConfig()
-        self._client = ElasticsearchConfig(config) \
-            if config == ElasticsearchConfig else ElasticsearchClient()
+        self._client = ElasticsearchClient(config) if isinstance(config, ElasticsearchConfig) else ElasticsearchClient()
 
         self._endpoint = self._client.config.es_hosts
 
     def _get_all_stats(self):
-        try:
-            request_json = self._client.rest(f'{self._endpoint}/_nodes/stats?pretty')
-            return json.loads(request_json)
-        except Exception as e:
-            self._log.error(f'Failed to get all stats: {e}')
-            return False
+        if self._client.connected:
+            try:
+                request_json = self._client.rest(f'{self._endpoint}/_nodes/stats?pretty')
+                return json.loads(request_json)
+            except Exception as e:
+                self._log.error(f'Failed to get all stats: {e}')
+                return False
+
+        return False
 
     def _get_all_jvm_metrics(self):
-        try:
-            return Cluster(self._get_all_stats())
-        except Exception as e:
-            self._log.error(f'Failed to get jvm metrics: {e}')
+        if self._client.connected:
+            try:
+                return Cluster(self._get_all_stats())
+            except Exception as e:
+                self._log.error(f'Failed to get jvm metrics: {e}')
+
+        return False
 
     def read_file(self, file):
         try:
@@ -38,7 +43,10 @@ class ElasticsearchJvmMonitor:
             self._log.error(f'Failed to read file: {e}')
 
     def get_metrics(self):
-        try:
-            return self._get_all_jvm_metrics()
-        except Exception as e:
-            self._log.error(f'Failed to get metrics: {e}')
+        if self._client.connected:
+            try:
+                return self._get_all_jvm_metrics()
+            except Exception as e:
+                self._log.error(f'Failed to get metrics: {e}')
+
+        return False
