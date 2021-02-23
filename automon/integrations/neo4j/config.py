ef0ae6c23319ff9ee0bc47139ec6c82d59b6e739
naisanza <naisanza@gmail.com>
2021-02-23 00:43:37 -0500

    add error logging to neo4j config

--- automon/integrations/neo4j/config.py
+++ automon/integrations/neo4j/config.py
@@ -1,5 +1,6 @@
 import os
 
+from automon.log.logger import Logging
 from automon.helpers.sanitation import Sanitation as S
 from automon.log.logger import Logging
 
@@ -8,6 +9,16 @@ log = Logging(name=__name__, level=Logging.DEBUG)
 
 class Neo4jConfig:
     def __init__(self):
-        self.user = os.getenv('NEO4J_USER')
-        self.password = os.getenv('NEO4J_PASSWORD')
-        self.hosts = S.list_from_string(os.getenv('NEO4J_SERVERS'))
+        self._log = Logging(name=Neo4jConfig.__name__, level=Logging.ERROR)
+        self.user = os.getenv('NEO4J_USER') or ''
+        self.password = os.getenv('NEO4J_PASSWORD') or ''
+        self.hosts = S.list_from_string(os.getenv('NEO4J_SERVERS')) or []
+
+        if not self.user:
+            self._log.error(f'NEO4J_USER not set')
+
+        if not self.password:
+            self._log.error(f'NEO4J_PASSWORD not set')
+
+        if not self.hosts:
+            self._log.error(f'NEO4J_SERVERS not set')
