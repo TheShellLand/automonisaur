fc6da7c649a31faf63b12faba765e276c6362232
naisanza <naisanza@gmail.com>
2021-02-23 22:55:33 -0500

    fixing neo4j tests

--- automon/tests/integrations/test_neo4j.py
+++ automon/tests/integrations/test_neo4j.py
@@ -5,9 +5,14 @@ from automon.integrations.neo4j.client import Neo4jClient
 
 
 class Neo4jTest(unittest.TestCase):
+    client = Neo4jClient()
 
-    def test_Neo4jWrapper(self):
-        self.assertTrue(Neo4jClient())
+    def test_Neo4jClient(self):
+        self.assertIsNotNone(Neo4jClient())
+
+    def test_send_data(self):
+        if self.client.connected:
+            self.assertTrue(self.client.send_data('human', {'name': 'eric'}))
 
 
 if __name__ == '__main__':
