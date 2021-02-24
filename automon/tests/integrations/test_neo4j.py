87d8ae3e17be6266d6c6b13347d8e872e71a99ef
naisanza <naisanza@gmail.com>
2021-02-23 21:17:18 -0500

    refactored neo4j client, add connection state

--- automon/tests/integrations/test_neo4j.py
+++ automon/tests/integrations/test_neo4j.py
@@ -1,14 +1,14 @@
 import json
 import unittest
 
-from automon.integrations.neo4j import Neo4jWrapper
+from automon.integrations.neo4j import Neo4jClient
 
 
 class Neo4jTest(unittest.TestCase):
 
     def test_Neo4jWrapper(self):
-        self.assertTrue(Neo4jWrapper)
-        self.assertTrue(Neo4jWrapper())
+        self.assertTrue(Neo4jClient())
 
-# if __name__ == '__main__':
-#     unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()
