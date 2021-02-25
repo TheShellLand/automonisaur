8981f0c5d3b232a5df90c10f05be1ba08f9ae940
naisanza <naisanza@gmail.com>
2021-02-24 23:30:19 -0500

    0.0.14
    Change log:
    
    hotfix neo4j
    
    - fix client always setting connection to false
    - fix node relationship to merge instead of create
    - fix delete all nodes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.0.13",
+    version="0.0.14",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
