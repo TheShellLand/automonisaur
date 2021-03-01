3c75cfc6b37cde2797877f9b2e6f89ca383d6e35
naisanza <naisanza@gmail.com>
2021-02-28 23:11:52 -0500

    add helpers
    - check cpu usage
    - http headers blobber
    - check if host if up

---
+++ automon/helpers/cpu.py
@@ -0,0 +1,16 @@
+import psutil
+
+from automon.log.logger import Logging
+
+log = Logging('cpu', level=Logging.DEBUG)
+
+
+def cpu_usage(max_cpu_percentage=80):
+    """Limit max cpu usage
+    """
+    if psutil.cpu_percent() < max_cpu_percentage:
+        log.debug(f'[{cpu_usage.__name__}] {psutil.cpu_percent()}%')
+        return True
+    else:
+        log.debug(f'[{cpu_usage.__name__}] {psutil.cpu_percent()}%')
+        return False
