99df710290f7a728c2bd32edc3175477ceda6de2
naisanza <naisanza@gmail.com>
2021-03-08 04:15:17 -0500

    0.0.19
    Change log:
    - add selenium library
    - update requirements
    - add openvpn
    - fix airport xml parsing issue
    - update unittest script to export env vars correctly
    - refactoring

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.0.18",
+    version="0.0.19",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
