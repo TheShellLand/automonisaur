30dd3edba850ef6c86b79b4e06cf74ce697ac586
naisanza <naisanza@gmail.com>
2021-03-19 12:10:59 -0400

    update selenium tests

--- automon/tests/integrations/test_selenium.py
+++ automon/tests/integrations/test_selenium.py
@@ -5,20 +5,31 @@ from automon.integrations.selenium.config import SeleniumConfig
 
 
 class SeleniumTest(unittest.TestCase):
+    b = SeleniumBrowser()
+
     def test_SeleniumConfig(self):
         self.assertTrue(SeleniumConfig())
 
     def test_SeleniumBrowser(self):
-        b = SeleniumBrowser()
 
-        if b.connected:
-            self.assertTrue(b)
-            self.assertFalse(b.get('http://555.555.555.555'))
-            self.assertTrue(b.get('http://google.com'))
-            self.assertTrue(b.get_screenshot_as_png())
-            self.assertTrue(b.get_screenshot_as_base64())
-            self.assertTrue(b.save_screenshot())
-            self.assertTrue(b.save_screenshot(folder='./'))
+        if self.b.connected:
+            self.assertFalse(self.b.get('http://555.555.555.555'))
+
+            self.assertTrue(self.b)
+            self.assertTrue(self.b.get('http://google.com'))
+            self.assertTrue(self.b.get_screenshot_as_png())
+            self.assertTrue(self.b.get_screenshot_as_base64())
+            self.assertTrue(self.b.save_screenshot())
+            self.assertTrue(self.b.save_screenshot(folder='./'))
+
+            self.assertIsNone(self.b.close())
+
+    def test_Neo4jSandbox(self):
+        if self.b.connected:
+            self.assertTrue(self.b.get('https://sandbox.neo4j.com/login'))
+            self.assertTrue()
+
+            self.assertIsNone(self.b.close())
 
 
 if __name__ == '__main__':
