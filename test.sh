9344107bc1652e9c0fab24915f42e004fb6196b4
naisanzaa <naisanza@gmail.com>
2021-04-20 13:56:54 -0400

    fix test script
    - add local testing
    - always upgrade pip

--- test.sh
+++ test.sh
@@ -4,8 +4,10 @@
 
 cd $(dirname $0) && set -xe
 
-DOCKERNAME=automon
-
-./build.sh
-
-docker run --rm $DOCKERNAME test "$@"
+if [ "$1" == "--local" ]; then
+  python3 -m pytest automon
+else
+  DOCKERNAME=automon
+  ./build.sh
+  docker run --rm $DOCKERNAME test "$@"
+fi
