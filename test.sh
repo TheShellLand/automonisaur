fb07c173efccc502119645e047fe1a28b948483d
naisanzaa <naisanza@gmail.com>
2021-04-21 12:12:45 -0400

    minor updates
    - add docker testing
    - add local testing
    - refactored dockerfile

--- test.sh
+++ test.sh
@@ -4,10 +4,4 @@
 
 cd $(dirname $0) && set -xe
 
-if [ "$1" == "--local" ]; then
-  python3 -m pytest automon
-else
-  DOCKERNAME=automon
-  ./build.sh
-  docker run --rm $DOCKERNAME test "$@"
-fi
+python3 -m pytest automon
