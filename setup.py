b53baed8a0d6c6b21d86e7272aea357d8f7b4a21
naisanzaa <naisanza@gmail.com>
2021-04-22 12:43:37 -0400

    0.0.22
    Change log:
    - fix scripts
    - fix ElasticsearchConfig logging error on missing hosts
    - minor updates
    - update docker build

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.0.21",
+    version="0.0.22",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
