56e1413f568bca20aa8cb1ad1564ec00203554a8
naisanzaa <naisanza@gmail.com>
2021-07-21 20:05:04 -0400

    update vds
    - prod-ready for ldap searching

--- automon/integrations/vds/client.py
+++ automon/integrations/vds/client.py
@@ -1,11 +1,17 @@
 import os
+import json
 import base64
 import requests
 
-from automon.integrations.vds.config import VdsConfig
+from .config import VdsConfig
+
+from queue import Queue
+
+from automon.log.logger import Logging
 
 
 class VdsLdapClient(object):
+    """Not implemented"""
     pass
 
 
@@ -14,46 +20,63 @@ class VdsRestClient(object):
         """VDS REST client"""
 
         self.config = config or VdsConfig()
+        self.records = Queue()
 
         # test connection
         self.connected = self.check_connection()
 
+        self._log = Logging(name=VdsRestClient.__name__, level=Logging.DEBUG)
+
     @staticmethod
     def check_connection():
+        """check if vds server is reacheable"""
         return False
 
-    def search(self, basedn: str, params: list = None):
-        return self._get(basedn, params)
+    def search(self, query: str = 'filter=cn=*', **kwargs):
+        """search ldap"""
+        return self._get(ldap_query=query, **kwargs)
 
-    def test(self, basedn: str = 'o=*', **kwargs):
-        return self._get(basedn=basedn, **kwargs)
+    def search_insecure(self, query: str = 'filter=cn=*', **kwargs):
+        """search ldap, ignoring ssl certificates"""
+        return self._get(ldap_query=query, verify=False, **kwargs)
 
-    def test_insecure(self, basedn: str = 'o=*', **kwargs):
-        return self._get(basedn=basedn, verify=False, **kwargs)
-
-    def test_cafile(self, basedn: str = 'o=*', cafile: str = None, **kwargs):
+    def search_cafile(self, query: str = 'filter=cn=*', cafile: str = None, **kwargs):
+        """search ldap, providing ssl certificate"""
 
         if os.path.exists(cafile):
-            return self._get(basedn=basedn, verify=cafile, **kwargs)
+            return self._get(ldap_query=query, verify=cafile, **kwargs)
 
         raise Exception(f'{cafile} not found')
 
-    def _get(self, basedn: str, params: list = None, **kwargs):
-        if params:
-            params = '?' + '&'.join(params)
+    def search_paging(self, start_index: int = 0, count: int = 1000):
+        """search ldap and page through all records"""
+
+        return
+
+    def _get(self, ldap_query: str = None, **kwargs):
+        """retrieve response from server"""
+        if ldap_query:
+            ldap_query = f'?{ldap_query}'
         else:
-            params = ''
+            ldap_query = ''
 
-        uri = f'{self.config.prot}://{self.config.server}:{self.config.port}/{self.config.path}'
-
-        basic_auth = base64.b64encode(f'{self.config.user}:{self.config.password}'.encode())
+        basic_auth = base64.b64encode(f'{self.config.user}:{self.config.password}'.encode()).decode()
 
         headers = {
             'Authorization': f'Basic {basic_auth}'
         }
 
-        url = f'{uri}/{basedn}{params}'
+        url = f'{self.config.uri}/{self.config.basedn}{ldap_query}'
 
         r = requests.get(url, headers=headers, **kwargs)
 
-        return r
+        [self._log.debug(f'results: {x}') for x in r.__dict__.items()]
+
+        if r.status_code != 200:
+            self._log.error(f'{url} {r.status_code} {r.reason}\n\n{r.content.decode()}')
+            ldap_result = False
+        else:
+            self._log.info(f'{url} {r.status_code} {r.reason}')
+            ldap_result = json.loads(r.content.decode())
+
+        return ldap_result
--- automon/integrations/vds/config.py
+++ automon/integrations/vds/config.py
@@ -1,10 +1,9 @@
 import os
 
+from automon.log.logger import Logging
+
 
 class VdsConfig(object):
-    dev = 'dev_vds'
-    prod = 'prod_vds'
-
     ldap_port = 636
     rest_port = 8090
 
@@ -15,13 +14,25 @@ class VdsConfig(object):
                  port: int = None,
                  path: str = None,
                  user: str = None,
-                 password: str = None):
+                 password: str = None,
+                 basedn: str = None):
         """VDS configuration"""
 
         self.prot = protocol or 'https'
-        self.server = server or os.getenv('VDS_SERVER') or self.dev
-        self.port = port or os.getenv('VDS_PORT') or self.rest_port or self.ldap_port
+        self.server = server or os.getenv('VDS_SERVER')
+        self.port = port or os.getenv('VDS_PORT') or self.rest_port
         self.path = path or self.rest_path
 
-        self.user = user or os.getenv('VDS_USER')
+        self.user = user or os.getenv('VDS_BIND_USER')
+        self.bind_user = self.user
         self.password = password or os.getenv('VDS_PASSWORD')
+        self.basedn = basedn or os.getenv('VDS_BASE_DN')
+
+        self.uri = f'{self.prot}://{self.server}:{self.port}/{self.path}'
+
+        self._log = Logging(name=VdsConfig.__name__, level=Logging.ERROR)
+
+        [self._log.debug(f'config: {x}') for x in self.__dict__.items()]
+
+    def __repr__(self):
+        return f'{self.prot}://{self.server}:{self.port} ({self.bind_user}) ({self.basedn})'
