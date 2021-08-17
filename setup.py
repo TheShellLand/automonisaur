2863681d835ad26c6ab5b83514e930e469cfd5d9
naisanzaa <naisanza@gmail.com>
2021-08-17 00:03:20 -0500

    0.0.25
    Change log:
    - fix splunk client
    - fix package url
    - update Logging
    - add pandas
    - add vds

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.0.24",
+    version="0.0.25",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
