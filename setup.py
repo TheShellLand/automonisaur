430a4bdae4eabd160b7667bc7799b44596c45221
naisanzaa <naisanza@gmail.com>
2021-08-24 23:59:06 -0400

    0.0.27
    Change log:
    - networking: add support when uri schema not included in url
    - networking: add logging, add tests
    - networking: add tests
    - logging: support filemode

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.0.26",
+    version="0.0.27",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
