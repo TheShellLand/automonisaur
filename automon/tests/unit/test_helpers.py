3dc9e78f06377118993e6fa2b49a15e55a38beef
naisanzaa <naisanza@gmail.com>
2021-08-24 23:57:45 -0400

    networking: add support when uri schema not included in url

--- automon/tests/unit/test_helpers.py
+++ automon/tests/unit/test_helpers.py
@@ -12,6 +12,7 @@ class NetworkingTest(unittest.TestCase):
         self.assertTrue(Networking.check_connection('https://www.google.com:443'))
         self.assertTrue(Networking.check_connection('x://www.google.com:443'))
         self.assertFalse(Networking.check_connection('x://localhost:0'))
+        self.assertFalse(Networking.check_connection('localhost:0'))
         self.assertTrue(Networking.urlparse('x://localhost:0'))
 
 
