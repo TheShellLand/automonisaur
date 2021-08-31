6edfcfe8eaa280fe08f2945af719288085b87679
naisanzaa <naisanza@gmail.com>
2021-08-31 15:47:08 -0400

    grok: build dataframe from pattern files

--- automon/helpers/grok/__init__.py
+++ automon/helpers/grok/__init__.py
@@ -1,11 +1,12 @@
 import os
-import re
+import pandas as pd
 
 from automon.log.logger import Logging
 
 
 class Grok:
     def __init__(self):
+        self._log = Logging(name=Grok.__name__, level=Logging.DEBUG)
 
         p = 'logstash-patterns-core/patterns'
         l = f'{os.path.join(os.path.split(os.path.realpath(__file__))[0])}'
@@ -13,31 +14,44 @@ class Grok:
         walk = os.walk(l)
 
         for folder, folders, files in walk:
+            f_list = [os.path.join(folder, f) for f in files]
+
             if 'legacy' in folder:
-                self.legacy = {f: self.file_to_dict(os.path.join(folder, f)) for f in files}
+                self.legacy_df = self.to_df(f_list)
+                self.legacy = self.legacy_df.to_dict()[0]
 
             if 'ecs-v1' in folder:
-                self.ecs_v1 = {f: self.file_to_dict(os.path.join(folder, f)) for f in files}
+                self.ecs_v1_df = self.to_df(f_list)
+                self.ecs_v1 = self.ecs_v1_df.to_dict()[0]
 
-        self._log = Logging(name=Grok.__name__, level=Logging.DEBUG)
+        pass
 
-    @staticmethod
-    def file_to_dict(file: str):
-        """convert logstash grok pattern file into dict
+    def to_df(self, files: list) -> pd.DataFrame:
 
-        :param file:
-        :return:
-        """
+        patterns = [self.to_pattern(f) for f in files]
+        return self.expanded_dict(patterns)
+
+    def to_pattern(self, file: str) -> pd.Series:
+        filename = os.path.split(file)[-1]
         pattern = open(file, 'r').read()
         pattern = [x for x in pattern.splitlines() if '#' not in x if x]
         pattern = [x.split(' ') for x in pattern]
         pattern = {k[0]: ''.join(k[1:]) for k in pattern}
 
-        return pattern
+        return pd.Series(pattern).rename(filename)
 
-    @staticmethod
-    def dict_to_pattern(dict):
-        return
+    def expanded_dict(self, patterns: list) -> pd.DataFrame:
+
+        # big_dict = {}
+        # for d in patterns:
+        #     for k, v in d.items():
+        #         if k not in big_dict.keys():
+        #             big_dict[k] = v
+        #         else:
+        #             self._log.info(f'skipping existing, {k}')
+
+        df = pd.DataFrame(pd.concat(patterns))
+        return df
 
 
 class GrokLegacy:
