1211d8524fc655decb86ed7499c72f1bd8132479
naisanzaa <naisanza@gmail.com>
2021-09-08 22:33:14 -0400

    cryptography: rename package

--- automon/helpers/http.py
+++ automon/helpers/http.py
@@ -1,13 +1,13 @@
 from automon.integrations.neo4j.cypher import Cypher
 
 
-# from core.helpers import crypto
+# from core.helpers import cryptography
 
 
 def http_header(headers):
     # [print(x) for x in auth.request_headers(request)]
 
-    # token = crypto.hash_key(sorted([x for x in headers]))
+    # token = cryptography.hash_key(sorted([x for x in headers]))
 
     args = dict(
         blob=sorted(headers),
