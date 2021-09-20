7f53d9c7be4fbb0c08d9e81b119d022b921e7a25
naisanzaa <naisanza@gmail.com>
2021-09-20 15:33:12 -0400

    crypto: update robinhood

--- automon/helpers/crypto/robinhood.py
+++ automon/helpers/crypto/robinhood.py
@@ -1,24 +1,28 @@
+from automon import Logging
 from automon.helpers.datascience import Pandas
 
 
 class Robinhood:
     def __init__(self, csv):
+        self._log = Logging(name=Robinhood.__name__, level=Logging.DEBUG)
+
+        self.csv = None
         self.df = None
         self.is_match = None
         self.type = Robinhood.__name__
-        self.csv = None
 
         r = RobinhoodCSV(csv)
         if r.matches:
-            self.df = r.df
+            self._log.info(f'matched {r}')
             self.is_match = True
             self.csv = csv
+            self.df = r.df
 
     def __repr__(self):
-        return self.csv
+        return f'{self.csv}'
 
     def __eq__(self, other):
-        if self.df == other.df:
+        if self.df.equals(other.df):
             return True
         return False
 
@@ -35,10 +39,14 @@ class RobinhoodCSV:
         ASSET NAME,RECEIVED DATE,COST BASIS(USD),DATE SOLD,PROCEEDS
 
         """
+        self._log = Logging(name=RobinhoodCSV.__name__, level=Logging.DEBUG)
+
+        self.csv = csv
         self.df = None
         self.matches = None
 
         if 'Provider: Robinhood Crypto LLC' in Pandas().read_csv(csv):
+            self._log.debug(f'matched {csv}')
             self.matches = True
 
             with open(csv) as f:
@@ -47,9 +55,12 @@ class RobinhoodCSV:
                 )
 
     def __repr__(self):
-        return RobinhoodCSV.__name__
+        return f'{self.df}'
 
-    def __eq__(self, other: Pandas):
-        if self.df == other.df:
-            return True
+    def __eq__(self, other):
+        if isinstance(other, RobinhoodCSV):
+            if self.df == other.df:
+                self._log.debug(f'same {other}')
+                return True
+            self._log.debug(f'different {other}')
         return False
