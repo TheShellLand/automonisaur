3ec4a218dd6e26b3db7290bf26e31c17cd0208f1
naisanzaa <naisanza@gmail.com>
2021-09-20 15:32:41 -0400

    crypto: update coinbase

--- automon/helpers/crypto/coinbase.py
+++ automon/helpers/crypto/coinbase.py
@@ -1,37 +1,41 @@
+from automon import Logging
 from automon.helpers.datascience import Pandas
 
 
 class Coinbase:
     def __init__(self, csv):
+        self._log = Logging(name=Coinbase.__name__, level=Logging.DEBUG)
+
+        self.csv = None
         self.df = None
         self.is_match = None
         self.type = Coinbase.__name__
-        self.csv = None
 
         r = CoinbaseCSV(csv)
         if r.matches:
-            self.df = r.df
             self.is_match = True
             self.csv = csv
+            self.df = r.df
 
     def __repr__(self):
         return self.csv
 
     def __eq__(self, other):
-        if self.df == other.df:
+        if self.df.equals(other.df):
             return True
         return False
 
 
 class CoinbaseCSV:
     def __init__(self, csv):
-        self.df = None
+        self.csv = csv
+        self.df = Pandas().read_csv(csv)
         self.matches = None
 
     def __repr__(self):
         return CoinbaseCSV.__name__
 
-    def __eq__(self, other: Pandas):
-        if self.df == other.df:
+    def __eq__(self, other):
+        if self.df.equals(other.df):
             return True
         return False
