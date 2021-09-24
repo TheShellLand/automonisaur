31eac5e7c6bbf9a5421758f8b19f4013b8cac309
naisanzaa <naisanza@gmail.com>
2021-09-24 14:41:57 -0400

    0.1.1
    Change log:
    - fix url in install.sh
    - grok: add GrokLegacy depreciation warning
    - grok: update tests
    - grok: build dataframe from pattern files
    - grok: updated for ecs-v1 compatibility

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.1.0",
+    version="0.1.1",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
