ada7559a76b5862c4366926969c1d3bcc574f185
naisanzaa <naisanza@gmail.com>
2021-09-24 01:20:31 -0400

    0.0.35
    Change log:
    - runner: initial commit
    - datascience: add pandas
    - crypto: add accounting for robinhood and regular csv
    - crypto: update accounting auto detect

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.0.34",
+    version="0.0.35",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
