03df5c589487f041dce82891604ef1e80a473bbd
naisanzaa <naisanza@gmail.com>
2021-10-01 11:05:32 -0400

    nmap: fix test to succeed if nmap isn't installed

--- automon/tests/integrations/test_nmap.py
+++ automon/tests/integrations/test_nmap.py
@@ -11,32 +11,36 @@ class NmapConfigTest(unittest.TestCase):
 class NmapTest(unittest.TestCase):
 
     def test_run(self):
-        self.assertTrue(Nmap().run('localhost'))
-        self.assertTrue(Nmap().run('localhost', output=False))
+        if Nmap().ready:
+            self.assertTrue(Nmap().run('localhost'))
+            self.assertTrue(Nmap().run('localhost', output=False))
 
     def test_nmap(self):
         nmap = Nmap()
 
-        self.assertTrue(Nmap().nmap(command='localhost'))
-        self.assertTrue(Nmap().nmap(command='localhost', output=False))
-        nmap.ready = False
-        self.assertFalse(nmap.nmap(command='localhosta'))
+        if Nmap().ready:
+            self.assertTrue(Nmap().nmap(command='localhost'))
+            self.assertTrue(Nmap().nmap(command='localhost', output=False))
+            nmap.ready = False
+            self.assertFalse(nmap.nmap(command='localhosta'))
 
     def test_scan(self):
         nmap = Nmap()
 
-        self.assertTrue(Nmap().scan(command='localhost'))
-        self.assertTrue(Nmap().scan(command='localhost', output=False))
-        nmap.ready = False
-        self.assertFalse(nmap.scan(command='localhosta'))
+        if Nmap().ready:
+            self.assertTrue(Nmap().scan(command='localhost'))
+            self.assertTrue(Nmap().scan(command='localhost', output=False))
+            nmap.ready = False
+            self.assertFalse(nmap.scan(command='localhosta'))
 
     def test_results(self):
         nmap = Nmap()
 
-        self.assertTrue(nmap.scan('-sn localhost'))
-        # self.assertTrue(nmap.scan(' 1.1.1.1'))
-        self.assertTrue(nmap.scan('-p 80,443 localhost'))
-        self.assertTrue(nmap.scan('-n -p 80,443 localhost localhost'))
+        if Nmap().ready:
+            self.assertTrue(nmap.scan('-sn localhost'))
+            # self.assertTrue(nmap.scan(' 1.1.1.1'))
+            self.assertTrue(nmap.scan('-p 80,443 localhost'))
+            self.assertTrue(nmap.scan('-n -p 80,443 localhost localhost'))
 
 
 if __name__ == '__main__':
