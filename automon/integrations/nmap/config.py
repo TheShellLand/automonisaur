65e9f49f51c7726bce93087173c890cce9f279ac
naisanzaa <naisanza@gmail.com>
2021-12-30 13:55:56 -0500

    nmap: checks if nmap is installed better

--- automon/integrations/nmap/config.py
+++ automon/integrations/nmap/config.py
@@ -7,19 +7,20 @@ class NmapConfig(object):
         self._log = Logging(name=NmapConfig.__name__, level=Logging.ERROR)
 
         self.nmap = None
-        self.ready = None
 
-        check = Run()
-        check.run('which nmap', **kwargs)
+    def __repr__(self):
+        if self.isReady():
+            return f'{self.nmap}'
+        return f'nmap not found'
+
+    def isReady(self, **kwargs):
+        check = Run('which nmap', **kwargs)
 
         if check.stdout:
             self.nmap = check.stdout.decode().strip()
-            self.ready = True
             self._log.debug(f'nmap located, {self.nmap}')
+            return True
         else:
             self._log.error(f'nmap not found', enable_traceback=False)
 
-    def __repr__(self):
-        if self.ready:
-            return f'{self.nmap}'
-        return f'nmap not found'
+        return False
