202ea991832dc70206a5d46fd4ede9ee0baf4dbb
naisanzaa <naisanza@gmail.com>
2021-12-31 05:05:51 -0500

    add sentryio

---
+++ automon/integrations/__init__.py
@@ -0,0 +1,1 @@
+from .sentryio import SentryClient
--- automon/log/logger.py
+++ automon/log/logger.py
@@ -4,6 +4,7 @@ import traceback
 from logging import DEBUG, INFO, WARN, ERROR, CRITICAL, NOTSET
 
 from automon.helpers import Dates
+from automon.integrations import SentryClient
 from automon.integrations.slack.slack_formatting import Emoji, Chat, Format
 
 logging.getLogger('logger').setLevel(CRITICAL)
@@ -92,35 +93,55 @@ class Logging(object):
             self.stream = LogStream() if log_stream else None
             logging.basicConfig(level=level, stream=self.stream)
 
+        self.sentry = SentryClient()
+
     def error(self, msg: any = None, enable_traceback: bool = True, raise_exception: bool = False):
         tb = traceback.format_exc()
         # tb = Chat.wrap(tb, Format.codeblock)
         if 'NoneType' not in tb and enable_traceback:
             self.logging.error(tb)
 
+        self.sentry.setLevel('error')
+        self.sentry.capture_message(msg)
+
         if msg and not raise_exception:
             return self.logging.error(msg)
 
         if raise_exception:
+            self.sentry.capture_exception()
             raise Exception(msg)
 
     def warning(self, msg: any):
+        self.sentry.setLevel('warning')
+        self.sentry.capture_message(msg)
+
         return self.logging.warning(msg)
 
     def warn(self, msg: any):
         return self.warning(msg)
 
     def info(self, msg: any):
+        self.sentry.setLevel('info')
+        self.sentry.capture_message(msg)
+
         return self.logging.info(msg)
 
     def debug(self, msg: any):
+        self.sentry.setLevel('debug')
+        self.sentry.capture_message(msg)
+
         return self.logging.debug(msg)
 
     def critical(self, msg: any):
+        self.sentry.setLevel('critical')
+        self.sentry.capture_message(msg)
+
         tb = traceback.format_exc()
         tb = Chat.wrap(tb, Format.codeblock)
         if 'NoneType' not in tb:
             self.logging.critical(tb)
+            self.sentry.capture_exception()
+
         return self.logging.critical(msg)
 
     @staticmethod
