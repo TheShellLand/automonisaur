34687791f2a73d597b87d0d01b5392dde956e710
naisanzaa <naisanza@gmail.com>
2022-01-04 05:58:18 -0500

    minio: update tests

--- automon/tests/integrations/test_minio_config.py
+++ automon/tests/integrations/test_minio_config.py
@@ -1,15 +1,11 @@
 import unittest
 
-from automon.integrations.minio.client import MinioClient
 from automon.integrations.minio.config import MinioConfig
 
 
-class MinioTest(unittest.TestCase):
+class Config(unittest.TestCase):
     def test_MinioConfig(self):
         self.assertTrue(MinioConfig())
-
-    def test_MinioClient(self):
-        self.assertTrue(MinioClient())
 
 
 if __name__ == '__main__':
