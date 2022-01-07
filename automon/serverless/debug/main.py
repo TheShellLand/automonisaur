7a01e74dd35f788e64bf1ea303d4dd2a5fade51b
naisanzaa <naisanza@gmail.com>
2022-01-06 20:53:37 -0500

    serverless: add request_json

--- automon/serverless/debug/main.py
+++ automon/serverless/debug/main.py
@@ -1,2 +1,9 @@
-def debug(requests):
+import json
+
+
+def request_dict(requests):
     return f'{requests.__dict__}'
+
+
+def request_as_json(requests):
+    return f"{json.dumps(requests.__dict__)}"
---
+++ automon/serverless/request_json/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from automon.log import Logging
+from automon.serverless import Requests
+
+log = Logging(__name__, level=Logging.DEBUG)
+
+
+def request_json(requests: Flask) -> bytes:
+    r = Requests(requests)
+    log.info(r)
+    return r.toJson()
