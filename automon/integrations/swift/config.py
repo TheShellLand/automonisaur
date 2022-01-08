840495f44de323f564e58d03086f268dd2736018
naisanzaa <naisanza@gmail.com>
2022-01-08 14:17:44 -0500

    config errors now warn instead

--- automon/integrations/swift/config.py
+++ automon/integrations/swift/config.py
@@ -20,31 +20,31 @@ class SwiftConfig:
         self.log = Logging(name=SwiftConfig.__name__, level=Logging.DEBUG)
 
         if not self.OS_USERNAME:
-            self.log.error(f'missing OS_USERNAME')
+            self.log.warn(f'missing OS_USERNAME')
         if not self.OS_PASSWORD:
-            self.log.error(f'missing OS_PASSWORD')
+            self.log.warn(f'missing OS_PASSWORD')
         if not self.OS_AUTH_URL:
-            self.log.error(f'missing OS_AUTH_URL')
+            self.log.warn(f'missing OS_AUTH_URL')
         if not self.OS_PROJECT_ID:
-            self.log.error(f'missing OS_PROJECT_ID')
+            self.log.warn(f'missing OS_PROJECT_ID')
         if not self.OS_PROJECT_NAME:
-            self.log.error(f'missing OS_PROJECT_NAME')
+            self.log.warn(f'missing OS_PROJECT_NAME')
         if not self.OS_USER_DOMAIN_NAME:
-            self.log.error(f'missing OS_USER_DOMAIN_NAME')
+            self.log.warn(f'missing OS_USER_DOMAIN_NAME')
         if not self.OS_PROJECT_DOMAIN_ID:
-            self.log.error(f'missing OS_PROJECT_DOMAIN_ID')
+            self.log.warn(f'missing OS_PROJECT_DOMAIN_ID')
         if not self.OS_REGION_NAME:
-            self.log.error(f'missing OS_REGION_NAME')
+            self.log.warn(f'missing OS_REGION_NAME')
         if not self.OS_INTERFACE:
-            self.log.error(f'missing OS_INTERFACE')
+            self.log.warn(f'missing OS_INTERFACE')
         if not self.OS_IDENTITY_API_VERSION:
-            self.log.error(f'missing OS_IDENTITY_API_VERSION')
+            self.log.warn(f'missing OS_IDENTITY_API_VERSION')
         if not self.SWIFTCLIENT_INSECURE:
-            self.log.error(f'missing SWIFTCLIENT_INSECURE')
+            self.log.warn(f'missing SWIFTCLIENT_INSECURE')
 
     def __eq__(self, other):
         if not isinstance(other, SwiftConfig):
-            self.log.error(f'Not implemented')
+            self.log.warn(f'Not implemented')
             return NotImplemented
 
         return self.OS_USERNAME == other.OS_USERNAME and \
