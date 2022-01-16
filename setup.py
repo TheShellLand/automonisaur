112e333e1a816b0ee6930b2242135187df7733ef
naisanzaa <naisanza@gmail.com>
2022-01-16 18:26:00 -0500

    0.2.1
    Breaking Changes:
    
    - moved external dependencies to integrations
    
    Change log:
    
    - slack: fix tests
    - slack: refactored async client
    - slack: removed AsyncSlackLogging
    - minor fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.2.0",
+    version="0.2.1",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
