88cf9680e498c501b679d62138cd4ef7a266f5d0
naisanzaa <naisanza@gmail.com>
2022-01-16 18:19:43 -0500

    slack: update SlackClient to config v2, update to nonasync

--- automon/integrations/slack/client.py
+++ automon/integrations/slack/client.py
@@ -1,27 +1,28 @@
 import os
 import slack
-import random
-import asyncio
 
 from automon.log import Logging
-from automon.helpers.asyncio_ import AsyncStarter
-from automon.integrations.slack.bots import BotInfo
-from automon.integrations.slack.error import SlackError
-from automon.integrations.slack.config import ConfigSlack
+
+from .config import SlackConfig
+from .bots import BotInfo
+from .error import SlackError
 
 
-class SlackClient(ConfigSlack):
-    """
-    All Slack interactions
-    """
+class SlackClient(SlackConfig):
 
-    def __init__(self, token: str = ConfigSlack.slack_token, username: str = '',
-                 channel: str = '', icon_emoji: str = None, icon_url: str = None):
+    def __init__(self, token: str = None,
+                 config: SlackConfig = None,
+                 username: str = None,
+                 channel: str = None,
+                 icon_emoji: str = None,
+                 icon_url: str = None):
+        """Slack client
+        """
 
         self._log = Logging(SlackClient.__name__, Logging.ERROR)
 
-        self.token = ConfigSlack.slack_token or token
-        self.client = slack.WebClient(token=token)
+        self.config = config or SlackConfig(token=token, username=username, channel=channel)
+        self.client = slack.WebClient(token=self.config.token)
 
         try:
             self.connected = True if self.client.auth_test() else False
@@ -29,19 +30,12 @@ class SlackClient(ConfigSlack):
             self.connected = False
 
         # TODO: use token to get bot info
-        self.username = ConfigSlack.slack_name or username or self._get_bot_info() or ''
-        self.channel = ConfigSlack.SLACK_DEFAULT_CHANNEL or channel or ''
+        self.username = self.config.username or self._get_bot_info() or ''
+        self.channel = self.config.SLACK_DEFAULT_CHANNEL
         self.icon_emoji = icon_emoji or ''
         self.icon_url = icon_url or ''
 
-        # start consumer
-        self._eventloop = AsyncStarter()
-        self.queue = self._eventloop.queue
-        self.task = self._eventloop.create_task(self._consumer())
-        self._stop = False
-
-        # TODO: integrate slacklog
-        # self.slacklog = SlackLogging(token)
+        self.queue = None
 
     def _get_bot_info(self):
         if not self.connected:
@@ -60,23 +54,27 @@ class SlackClient(ConfigSlack):
 
         return ''
 
-    def _run_until_complete(self):
-        while True:
-            if self.queue.qsize():
-                asyncio.run(asyncio.sleep(0))
-            else:
-                break
-        self._stop = True
+    def chat_postMessage(self, channel: str, text: str) -> slack.WebClient.chat_postMessage:
 
-    async def chat_postMessage(self, channel: str, text: str) -> slack.WebClient.chat_postMessage:
-
-        if not self.connected:
+        if self.connected:
             return False
 
         if not text:
             return SyntaxError
 
-        await self.queue.put((channel, text))
+        msg = f'{channel} @{self.username}: {text}'
+        self._log.debug(msg)
+
+        try:
+            response = self.client.chat_postMessage(
+                text=text, channel=channel, username=self.username,
+                icon_emoji=self.icon_emoji, icon_url=self.icon_url)
+            assert response["ok"]
+            return response
+        except Exception as e:
+            self._log.error(e, enable_traceback=False)
+
+        return False
 
     def files_upload(self, file, filename=None):
         """
@@ -118,54 +116,6 @@ class SlackClient(ConfigSlack):
             file=file, filename=filename, title=title, username=self.username, channels=self.channel)
 
         assert response["ok"]
-
         self._log.debug(f'File uploaded: {file} ({file_size}B) ({self.username}')
 
         return response
-
-    async def _consumer(self):
-
-        burst = 0
-        burst_max = 0
-        retry = 0
-        while self.connected:
-            channel, text = await self.queue.get()
-
-            if self._stop:
-                break
-
-            msg = f'{channel} @{self.username}: {text}'
-
-            while self.connected:
-                try:
-                    self._log.debug(msg)
-                    response = self.client.chat_postMessage(
-                        text=text, channel=channel, username=self.username,
-                        icon_emoji=self.icon_emoji, icon_url=self.icon_url)
-                    assert response["ok"]
-
-                    if not retry and self.queue.qsize() > 10:
-                        # if a lot in the queue, try bursting
-                        await asyncio.sleep(random.choice(range(2)))
-                    else:
-                        sleep = random.choice(range(4))
-                        self._log.debug(f'sleeping {sleep}, queue size is {self.queue.qsize()}')
-                        await asyncio.sleep(sleep)
-
-                    self._log.debug(f'Burst: {burst}, Retry: {retry}, Queue {self.queue.qsize()}')
-
-                    burst += 1
-                    retry = 0
-
-                    break
-                except Exception as e:
-                    if retry > 5:
-                        break
-
-                    retry += 1
-                    burst_max = burst
-                    error = SlackError(e)
-                    self._log.error(
-                        f'{self._consumer.__name__}\t{error.error}\t{msg}\tRetry: {retry}, Burst max: {burst_max}',
-                        enable_traceback=False)
-                    burst = 0
