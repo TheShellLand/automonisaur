10c990f27f67db12cd25e05c168d113feeeb7648
naisanzaa <naisanza@gmail.com>
2022-01-21 19:23:08 -0500

    elasticsearch: update config, logs if connected, minor refactoring

--- automon/integrations/elasticsearch/jvm.py
+++ automon/integrations/elasticsearch/jvm.py
@@ -13,7 +13,7 @@ class ElasticsearchJvmMonitor:
         self._config = config if isinstance(config, ElasticsearchConfig) else ElasticsearchConfig()
         self._client = ElasticsearchClient(config) if isinstance(config, ElasticsearchConfig) else ElasticsearchClient()
 
-        self._endpoint = self._client._config.es_hosts
+        self._endpoint = self._client.config.ELASTICSEARCH_HOSTS
 
     def _get_all_stats(self):
         if self._client.connected():
