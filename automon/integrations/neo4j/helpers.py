c04c5fb59a6416c7eae4c72e9c35ecfa62fd1a24
naisanzaa <naisanza@gmail.com>
2022-01-24 00:51:12 -0500

    neo4j: refactor Results

--- automon/integrations/neo4j/helpers.py
+++ automon/integrations/neo4j/helpers.py
@@ -4,27 +4,23 @@ from neo4j.work.result import Result
 
 from automon.log import Logging
 
-log = Logging(name=__name__, level=Logging.DEBUG)
+log = Logging(name='Results', level=Logging.DEBUG)
 
 
 class Results(Result):
     def __init__(self, results):
         self._results = results
+        self.summary = results._summary
 
-        try:
-            self._summary = results._summary
-            self.counters = self._summary.counters
-            self.notifications = self._summary.notifications
-            self.query = self._summary.query
-            self.query_type = self._summary.query_type
+        if self.summary:
+            self.counters = self.summary.counters
+            self.notifications = self.summary.notifications
+            self.query = self.summary.query
+            self.query_type = self.summary.query_type
+            self.metadata = self.summary.metadata
 
-            self.metadata = self._summary.metadata
-            self.stats = self.metadata.get('stats', None)
-        except Exception as e:
-            log.error(e, enable_traceback=False)
+            if self.metadata:
+                self.stats = self.metadata.get('stats', None)
 
     def __str__(self):
-        try:
-            return f'{self.stats}'
-        except:
-            return 'no nodes'
+        return f'{self.__dict__}'
