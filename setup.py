7eb981fc28ca5c3ffcd40bfc110791227f6d826b
naisanzaa <naisanza@gmail.com>
2022-01-24 22:48:45 -0500

    0.2.3
    Change log:
    
    - neo4j: refactored a bunch of things
    - neo4j: env var NEO4J_HOST
    - helpers: add asyncio
    - logger: minor update
    - elasticsearch: logs client connected
    - minor updates
    - disable 3.6 test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.2.2",
+    version="0.2.3",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
