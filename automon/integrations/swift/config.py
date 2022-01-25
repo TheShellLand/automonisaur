dd68dd76810e2ff81915b7503a38c7285313105b
naisanzaa <naisanza@gmail.com>
2022-01-24 22:58:36 -0500

    update openstack env vars

--- automon/integrations/swift/config.py
+++ automon/integrations/swift/config.py
@@ -4,41 +4,41 @@ from automon.log import Logging
 
 
 class SwiftConfig:
-    OS_USERNAME = os.getenv('OS_USERNAME') or ''
-    OS_PASSWORD = os.getenv('OS_PASSWORD') or ''
-    OS_AUTH_URL = os.getenv('OS_AUTH_URL') or ''
-    OS_PROJECT_ID = os.getenv('OS_PROJECT_ID') or ''
-    OS_PROJECT_NAME = os.getenv('OS_PROJECT_NAME') or ''
-    OS_USER_DOMAIN_NAME = os.getenv('OS_USER_DOMAIN_NAME') or ''
-    OS_PROJECT_DOMAIN_ID = os.getenv('OS_PROJECT_DOMAIN_ID') or ''
-    OS_REGION_NAME = os.getenv('OS_REGION_NAME') or 'RegionOne'
-    OS_INTERFACE = os.getenv('OS_INTERFACE') or 'public'
-    OS_IDENTITY_API_VERSION = os.getenv('OS_IDENTITY_API_VERSION') or '3'
+    OPENSTACK_USERNAME = os.getenv('OPENSTACK_USERNAME') or ''
+    OPENSTACK_PASSWORD = os.getenv('OPENSTACK_PASSWORD') or ''
+    OPENSTACK_AUTH_URL = os.getenv('OPENSTACK_AUTH_URL') or ''
+    OPENSTACK_PROJECT_ID = os.getenv('OPENSTACK_PROJECT_ID') or ''
+    OPENSTACK_PROJECT_NAME = os.getenv('OPENSTACK_PROJECT_NAME') or ''
+    OPENSTACK_USER_DOMAIN_NAME = os.getenv('OPENSTACK_USER_DOMAIN_NAME') or ''
+    OPENSTACK_PROJECT_DOMAIN_ID = os.getenv('OPENSTACK_PROJECT_DOMAIN_ID') or ''
+    OPENSTACK_REGION_NAME = os.getenv('OPENSTACK_REGION_NAME') or 'RegionOne'
+    OPENSTACK_INTERFACE = os.getenv('OPENSTACK_INTERFACE') or 'public'
+    OPENSTACK_IDENTITY_API_VERSION = os.getenv('OPENSTACK_IDENTITY_API_VERSION') or '3'
     SWIFTCLIENT_INSECURE = os.getenv('SWIFTCLIENT_INSECURE') or 'True'
 
     def __init__(self):
         self.log = Logging(name=SwiftConfig.__name__, level=Logging.DEBUG)
 
-        if not self.OS_USERNAME:
-            self.log.warn(f'missing OS_USERNAME')
-        if not self.OS_PASSWORD:
-            self.log.warn(f'missing OS_PASSWORD')
-        if not self.OS_AUTH_URL:
-            self.log.warn(f'missing OS_AUTH_URL')
-        if not self.OS_PROJECT_ID:
-            self.log.warn(f'missing OS_PROJECT_ID')
-        if not self.OS_PROJECT_NAME:
-            self.log.warn(f'missing OS_PROJECT_NAME')
-        if not self.OS_USER_DOMAIN_NAME:
-            self.log.warn(f'missing OS_USER_DOMAIN_NAME')
-        if not self.OS_PROJECT_DOMAIN_ID:
-            self.log.warn(f'missing OS_PROJECT_DOMAIN_ID')
-        if not self.OS_REGION_NAME:
-            self.log.warn(f'missing OS_REGION_NAME')
-        if not self.OS_INTERFACE:
-            self.log.warn(f'missing OS_INTERFACE')
-        if not self.OS_IDENTITY_API_VERSION:
-            self.log.warn(f'missing OS_IDENTITY_API_VERSION')
+        if not self.OPENSTACK_USERNAME:
+            self.log.warn(f'missing OPENSTACK_USERNAME')
+        if not self.OPENSTACK_PASSWORD:
+            self.log.warn(f'missing OPENSTACK_PASSWORD')
+        if not self.OPENSTACK_AUTH_URL:
+            self.log.warn(f'missing OPENSTACK_AUTH_URL')
+        if not self.OPENSTACK_PROJECT_ID:
+            self.log.warn(f'missing OPENSTACK_PROJECT_ID')
+        if not self.OPENSTACK_PROJECT_NAME:
+            self.log.warn(f'missing OPENSTACK_PROJECT_NAME')
+        if not self.OPENSTACK_USER_DOMAIN_NAME:
+            self.log.warn(f'missing OPENSTACK_USER_DOMAIN_NAME')
+        if not self.OPENSTACK_PROJECT_DOMAIN_ID:
+            self.log.warn(f'missing OPENSTACK_PROJECT_DOMAIN_ID')
+        if not self.OPENSTACK_REGION_NAME:
+            self.log.warn(f'missing OPENSTACK_REGION_NAME')
+        if not self.OPENSTACK_INTERFACE:
+            self.log.warn(f'missing OPENSTACK_INTERFACE')
+        if not self.OPENSTACK_IDENTITY_API_VERSION:
+            self.log.warn(f'missing OPENSTACK_IDENTITY_API_VERSION')
         if not self.SWIFTCLIENT_INSECURE:
             self.log.warn(f'missing SWIFTCLIENT_INSECURE')
 
@@ -47,9 +47,9 @@ class SwiftConfig:
             self.log.warn(f'Not implemented')
             return NotImplemented
 
-        return self.OS_USERNAME == other.OS_USERNAME and \
-               self.OS_PASSWORD == other.OS_PASSWORD and \
-               self.OS_AUTH_URL == other.OS_AUTH_URL and \
-               self.OS_PROJECT_ID == other.OS_PROJECT_ID and \
-               self.OS_PROJECT_NAME == other.OS_PROJECT_NAME and \
-               self.OS_PROJECT_DOMAIN_ID == other.OS_PROJECT_DOMAIN_ID
+        return self.OPENSTACK_USERNAME == other.OPENSTACK_USERNAME and \
+               self.OPENSTACK_PASSWORD == other.OPENSTACK_PASSWORD and \
+               self.OPENSTACK_AUTH_URL == other.OPENSTACK_AUTH_URL and \
+               self.OPENSTACK_PROJECT_ID == other.OPENSTACK_PROJECT_ID and \
+               self.OPENSTACK_PROJECT_NAME == other.OPENSTACK_PROJECT_NAME and \
+               self.OPENSTACK_PROJECT_DOMAIN_ID == other.OPENSTACK_PROJECT_DOMAIN_ID
