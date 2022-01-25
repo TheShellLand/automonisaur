023e2c41db10b94a2a756168f40e2344f707a08d
naisanzaa <naisanza@gmail.com>
2022-01-25 06:19:38 -0500

    0.2.4
    Change log:
    
    - neo4j: add Node class to handle all Nodes
    - neo4j: fixed tests
    - minor fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.2.3",
+    version="0.2.4",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
