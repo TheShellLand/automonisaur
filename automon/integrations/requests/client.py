8c9912350657cdbbc862cd48370a6c6aeed3df39
naisanzaa <naisanza@gmail.com>
2022-01-25 19:20:18 -0500

    requests: add delete, patch, put

--- automon/integrations/requests/client.py
+++ automon/integrations/requests/client.py
@@ -23,6 +23,22 @@ class RequestsClient(object):
             self.url = url
             self.results = self.get(url=self.url, data=self.data, headers=self.headers)
 
+    def delete(self,
+               url: str = None,
+               data: dict = None,
+               headers: dict = None, **kwargs) -> bool:
+        """requests.delete"""
+
+        try:
+            self.results = requests.delete(url=url, data=data, headers=headers, **kwargs)
+            self._log.debug(f'{self.results.status_code} '
+                            f'{self.results.url} '
+                            f'{round(len(self.results.content) / 1024, 2)}  KB')
+            return True
+        except Exception as e:
+            self._log.error(f'{self.results.status_code} get failed. {e}', enable_traceback=False)
+        return False
+
     def get(self,
             url: str = None,
             data: dict = None,
@@ -39,6 +55,22 @@ class RequestsClient(object):
             self._log.error(f'{self.results.status_code} get failed. {e}', enable_traceback=False)
         return False
 
+    def patch(self,
+              url: str = None,
+              data: dict = None,
+              headers: dict = None, **kwargs) -> bool:
+        """requests.patch"""
+
+        try:
+            self.results = requests.patch(url=url, data=data, headers=headers, **kwargs)
+            self._log.debug(f'{self.results.status_code} '
+                            f'{self.results.url} '
+                            f'{round(len(self.results.content) / 1024, 2)}  KB')
+            return True
+        except Exception as e:
+            self._log.error(f'{self.results.status_code} post failed. {e}', raise_exception=False)
+        return False
+
     def post(self,
              url: str = None,
              data: dict = None,
@@ -47,6 +79,22 @@ class RequestsClient(object):
 
         try:
             self.results = requests.post(url=url, data=data, headers=headers, **kwargs)
+            self._log.debug(f'{self.results.status_code} '
+                            f'{self.results.url} '
+                            f'{round(len(self.results.content) / 1024, 2)}  KB')
+            return True
+        except Exception as e:
+            self._log.error(f'{self.results.status_code} post failed. {e}', raise_exception=False)
+        return False
+
+    def put(self,
+            url: str = None,
+            data: dict = None,
+            headers: dict = None, **kwargs) -> bool:
+        """requests.put"""
+
+        try:
+            self.results = requests.put(url=url, data=data, headers=headers, **kwargs)
             self._log.debug(f'{self.results.status_code} '
                             f'{self.results.url} '
                             f'{round(len(self.results.content) / 1024, 2)}  KB')
