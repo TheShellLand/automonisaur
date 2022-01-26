287a7aecb74f56bf0126e47e4e0c80b9cea2760e
naisanzaa <naisanza@gmail.com>
2022-01-25 19:22:04 -0500

    gmail: add basic client and config
    still missing:
    
    - requests body to sent
    - messages
    - settings
    - threads
    
    docs: https://developers.google.com/gmail/api/reference/rest

---
+++ automon/integrations/google/gmail/__init__.py
@@ -0,0 +1,2 @@
+from .v1.client import GmailClient as GmailClientV1
+from .v1.config import GmailConfig as GmailConfigV1
---
+++ automon/integrations/google/gmail/v1/config.py
@@ -0,0 +1,20 @@
+from os import getenv
+
+from automon import Logging
+
+log = Logging(name='GmailConfig', level=Logging.DEBUG)
+
+
+class GmailConfig:
+    def __init__(self, endpoint: str = None,
+                 api_key: str = None,
+                 user: str = None,
+                 password: str = None):
+        """Gmail config"""
+        self.endpoint = endpoint or getenv('GMAIL_ENDPOINT') or 'https://gmail.googleapis.com'
+        self.api_key = api_key or getenv('GMAIL_API_KEY')
+        self.userId = user or getenv('GMAIL_USER')
+        self.password = password or getenv('GMAIL_PASSWORD')
+
+    def __repr__(self):
+        return f'{self.__dict__}'
