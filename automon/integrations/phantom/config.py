e25088e4c4ced0c1c3458f5c919853f1e8178b44
naisanzaa <naisanza@gmail.com>
2022-02-01 15:18:52 -0700

    add phantom client and config and rest urls

---
+++ automon/integrations/phantom/config.py
@@ -0,0 +1,23 @@
+from automon import Logging
+from automon.helpers.os import environ
+
+log = Logging(name='PhantomConfig', level=Logging.DEBUG)
+
+
+class PhantomConfig:
+    def __init__(self, host: str = None,
+                 user: str = None,
+                 password: str = None,
+                 auth_token: str = None):
+        """Phantom Config"""
+
+        self.host = host or environ('PHANTOM_HOST')
+        self.user = user or environ('PHANTOM_USER')
+        self.password = password or environ('PHANTOM_PASSWORD')
+        self.auth = (self.user, self.password)
+        self.auth_token = auth_token or environ('PHANTOM_AUTH_TOKEN')
+
+        self.headers = {'ph-auth-token': self.auth_token}
+
+    def __repr__(self):
+        return f'{self.__dict__}'
