1fd425f5dfb541b09041f8394d6bea36dcd48e8a
naisanzaa <naisanza@gmail.com>
2022-02-05 19:56:38 -0700

    airport: test to neo4j

---
+++ automon/tests/integrations/mac/test_airport_neo4j.py
@@ -0,0 +1,19 @@
+import sys
+import unittest
+
+from automon.integrations.mac.airport import Airport
+
+from automon.integrations.neo4j import Neo4jClient
+
+
+class AirportToNeo4jTest(unittest.TestCase):
+    a = Airport()
+    n = Neo4jClient()
+
+    def test_scan_xml(self):
+        if self.a.is_mac:
+            self.assertTrue(self.a.scan_xml())
+
+
+if __name__ == '__main__':
+    unittest.main()
