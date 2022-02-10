73b463816fb4042e3caff5367c200d333e75ed89
naisanzaa <naisanza@gmail.com>
2022-02-10 04:05:04 -0500

    airport: evaluate nested properties correctly

--- automon/integrations/mac/airport/ssid.py
+++ automon/integrations/mac/airport/ssid.py
@@ -1,11 +1,10 @@
-import collections
-
 from automon.log import Logging
+
+log = Logging(name='Ssid', level=Logging.DEBUG)
 
 
 class Ssid:
     def __init__(self, ssid: dict):
-        self._log = Logging(name=Ssid.__name__, level=Logging.DEBUG)
         self._ssid = dict(sorted(ssid.items()))
 
         self.AGE = int(self._ssid.get('AGE', 0))
@@ -21,44 +20,40 @@ class Ssid:
         self.NOISE = int(self._ssid.get('NOISE', 0))
         self.RATES = self._ssid.get('RATES', None)
         self.RSSI = int(self._ssid.get('RSSI', 0))
-        self.SSID = self._ssid.get('SSID', None)
+        self.SSID_B64 = self._ssid.get('SSID', None)
         self.SSID_STR = self._ssid.get('SSID_STR', None)
 
-        self.age = self.AGE
-        self.ap_mode = self.AP_MODE
-        self.bssid = self.BSSID
-        self.channel = self.CHANNEL
-        self.channel_offset = self.HT_SECONDARY_CHAN_OFFSET
-        self.channel_offset_full = f'{self.channel},+{self.channel_offset}'
-        self.channels = self.IE_KEY_80211D_NUM_CHANNELS
-        self.country = self.IE_KEY_80211D_COUNTRY_CODE
-        self.device_info = self.INFO
-        self.distance = self.RSSI
-        self.mac = self.BSSID
-        self.max_power = self.IE_KEY_80211D_MAX_POWER
-        self.noise = self.NOISE
-        self.power = self.RSSI
-        self.rates = self.RATES
-        self.rssi = self.RSSI
-        self.ssid = self.SSID_STR
-        self.wps_state = self.IE_KEY_WPS_SC_STATE
+        # custom
+        self.CHANNEL_OFFSET = self.HT_SECONDARY_CHAN_OFFSET
+        self.CHANNEL_OFFSET_FULL = f'{self.CHANNEL},+{self.CHANNEL_OFFSET}'
+        self.CHANNELS = self.IE_KEY_80211D_NUM_CHANNELS
+        self.COUNTRY = self.IE_KEY_80211D_COUNTRY_CODE
+        self.DEVICE_INFO = self.INFO
+        self.DISTANCE = self.RSSI
+        self.MAC = self.BSSID
+        self.MAX_POWER = self.IE_KEY_80211D_MAX_POWER
+        self.POWER = self.RSSI
+        self.SSID = self.SSID_STR
+        self.WPS_STATE = self.IE_KEY_WPS_SC_STATE
 
-        self.summary = f'[rssi: {self.distance} dBm] ' \
-                       f'[ch: {self.channel}] ' \
-                       f'{self.ssid} ' \
-                       f'[bssid: {self.mac}] ' \
-                       f'[noise: {self.noise} dBm] ' \
-                       f'[age: {self.age}] '
-
-        self._log.debug(f'{self.summary}')
+        log.debug(f'{self.summary}')
 
     def __repr__(self):
         return f'{self.summary}'
 
     def __eq__(self, other):
         if isinstance(other, Ssid):
-            return self.bssid == other.bssid
+            return self.BSSID == other.BSSID
 
     def __lt__(self, other):
         if isinstance(other, Ssid):
-            return self.rssi < other.rssi
+            return self.RSSI < other.RSSI
+
+    @property
+    def summary(self):
+        return f'[rssi: {self.DISTANCE} dBm] ' \
+               f'[ch: {self.CHANNEL}] ' \
+               f'{self.SSID} ' \
+               f'[bssid: {self.MAC}] ' \
+               f'[noise: {self.NOISE} dBm] ' \
+               f'[age: {self.AGE}] '
