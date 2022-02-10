33eb0b834b486b8ae03128899f509d6815cf40a1
naisanzaa <naisanza@gmail.com>
2022-02-10 04:05:25 -0500

    airport: update tests

--- automon/tests/integrations/airport/test_airport.py
+++ automon/tests/integrations/airport/test_airport.py
@@ -8,40 +8,40 @@ class AirportTest(unittest.TestCase):
     a = Airport()
 
     def test_run(self):
-        if self.a.is_mac:
+        if self.a.isReady():
             self.assertTrue(self.a.run())
 
     def test_scan(self):
-        if self.a.is_mac:
+        if self.a.isReady():
             self.assertTrue(self.a.scan())
             self.assertTrue(self.a.scan(0))
 
     def test_summary(self):
-        if self.a.is_mac:
+        if self.a.isReady():
             self.assertTrue(self.a.scan_summary())
             self.assertTrue(self.a.scan_summary(0))
 
     def test_xml(self):
-        if self.a.is_mac:
+        if self.a.isReady():
             self.assertTrue(self.a.scan_xml())
             self.assertTrue(self.a.scan_xml(0))
 
     def test_set_channel(self):
-        if self.a.is_mac:
+        if self.a.isReady():
             pass
             # self.assertTrue(self.a.set_channel(10))
 
     def test_disassociate(self):
-        if self.a.is_mac:
+        if self.a.isReady():
             pass
             # self.assertTrue(self.a.disassociate())
 
     def test_getinto(self):
-        if self.a.is_mac:
+        if self.a.isReady():
             self.assertTrue(self.a.getinfo())
 
     def test_create_psk(self):
-        if self.a.is_mac:
+        if self.a.isReady():
             self.assertTrue(self.a.create_psk(ssid='AAAAAAAA', passphrase='CALVIN'))
 
 
