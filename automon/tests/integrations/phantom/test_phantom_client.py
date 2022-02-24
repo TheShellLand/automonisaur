04c8f4e9f1b1b1b1a402c42301920d79770cf2fc
naisanzaa <naisanza@gmail.com>
2022-02-24 14:38:00 -0500

    phantom: tests

--- automon/tests/integrations/phantom/test_phantom_client.py
+++ automon/tests/integrations/phantom/test_phantom_client.py
@@ -2,37 +2,37 @@ import unittest
 
 from automon.integrations.phantom import PhantomClient
 
+c = PhantomClient()
+
 
 class TestPhantomClient(unittest.TestCase):
-    c = PhantomClient()
 
     def test_isConnected(self):
-        if self.c.isConnected():
-            self.assertTrue(self.c.isConnected())
+        if c.isConnected():
+            self.assertTrue(c.isConnected())
         else:
-            self.assertFalse(self.c.isConnected())
+            self.assertFalse(c.isConnected())
 
     def test_list_containers(self):
-        if self.c.isConnected():
-            self.assertTrue(self.c.list_containers())
+        if c.isConnected():
+            self.assertTrue(c.list_containers())
         else:
-            self.assertFalse(self.c.list_containers())
+            self.assertFalse(c.list_containers())
 
     def test_delete_containers(self):
         container = 0
 
-        if self.c.isConnected():
-            self.assertTrue(self.c.delete_containers(identifier=container))
+        if c.isConnected():
+            self.assertTrue(c.delete_containers(identifier=container))
         else:
-            self.assertFalse(self.c.delete_containers(identifier=container))
+            self.assertFalse(c.delete_containers(identifier=container))
 
     def test_create_container(self):
-        container = {}
 
-        if self.c.isConnected():
-            self.assertTrue(self.c.create_container(container))
+        if c.isConnected():
+            self.assertTrue(c.create_container(label='testing', name='AAAA'))
         else:
-            self.assertFalse(self.c.create_container(container))
+            self.assertFalse(c.create_container(label='testing', name='AAAA'))
 
 
 if __name__ == '__main__':
