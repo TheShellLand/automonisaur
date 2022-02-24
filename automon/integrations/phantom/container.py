f9af7435ae9e554f2d9bcc2c1058e015eaf5b8a4
naisanzaa <naisanza@gmail.com>
2022-02-24 14:15:05 -0500

    phantom: can compare container age

--- automon/integrations/phantom/container.py
+++ automon/integrations/phantom/container.py
@@ -1,4 +1,11 @@
 import json
+import datetime
+
+from dateutil import parser
+
+from automon import Logging
+
+log = Logging('Container', level=Logging.CRITICAL)
 
 
 class Container:
@@ -10,17 +17,32 @@ class Container:
 
     def __repr__(self):
         if self.__len__() > 100:
-            return f'{{' \
-                   f'name={self.name}, ' \
-                   f'label={self.label}, ' \
-                   f'status={self.status}, ' \
-                   f'id={self.id}, ' \
-                   f'artifacts={self.artifact_count}, ' \
-                   f'created={self.start_time}}} '
+            return json.dumps(dict(
+                name=self.name,
+                label=self.label,
+                status=self.status,
+                id=self.id,
+                artifact_count=self.artifact_count,
+                start_time=self.start_time
+            ))
         return self.to_json()
 
     def __len__(self):
         return len(self.to_json())
+
+    def __lt__(self, other):
+        return self._created() < other._created()
+
+    def _parse_timestamp(self, timestamp: str) -> datetime.datetime:
+        """parse string into datetime object
+
+        :timestamp: '2020-02-18T15:50:58.903568Z'
+        :returns: datetime.datetime(2020, 2, 18, 15, 50, 58, 903568, tzinfo=tzutc())
+        """
+        return parser.parse(timestamp)
+
+    def _created(self):
+        return self._parse_timestamp(self.start_time)
 
     def to_dict(self):
         return {k: v for k, v in self.__dict__.items() if v}
