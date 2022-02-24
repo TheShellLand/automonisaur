21e7a8a33c9910209f1be0e9d4078b73a15d5af7
naisanzaa <naisanza@gmail.com>
2022-02-24 13:36:50 -0500

    phantom: update Container

--- automon/integrations/phantom/container.py
+++ automon/integrations/phantom/container.py
@@ -1,11 +1,29 @@
+import json
+
+
 class Container:
     def __init__(self, container: dict):
+        self.artifact_count = None
+        self.start_time = None
+        self.id = None
         self.__dict__.update(container)
 
     def __repr__(self):
-        return f'' \
-               f'name={self.name}, ' \
-               f'status={self.status}, ' \
-               f'id={self.id}, ' \
-               f'artifacts={self.artifact_count}, ' \
-               f'created={self.start_time} '
+        if self.__len__() > 100:
+            return f'{{' \
+                   f'name={self.name}, ' \
+                   f'label={self.label}, ' \
+                   f'status={self.status}, ' \
+                   f'id={self.id}, ' \
+                   f'artifacts={self.artifact_count}, ' \
+                   f'created={self.start_time}}} '
+        return self.to_json()
+
+    def __len__(self):
+        return len(self.to_json())
+
+    def to_dict(self):
+        return {k: v for k, v in self.__dict__.items() if v}
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
