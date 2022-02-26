df020c4606da2a3c2d232b6f2a3a49748500d753
naisanzaa <naisanza@gmail.com>
2022-02-26 17:59:18 -0500

    minio: client tests

--- automon/tests/integrations/minio/test_minio_client_public.py
+++ automon/tests/integrations/minio/test_minio_client_public.py
@@ -1,31 +1,35 @@
 import unittest
 import hashlib
 
-from automon.integrations.minio.client import MinioClient
+from automon.integrations.minio import MinioClient
+from automon.integrations.minio.bucket import Bucket
 
-c = MinioClient()
+MINIO_ENDPOINT = 'play.minio.io:9000'
+MINIO_ACCESS_KEY = 'Q3AM3UQ867SPQQA43P2F'
+MINIO_SECRET_KEY = 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
+
+c = MinioClient(endpoint=MINIO_ENDPOINT, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET_KEY)
+bucket = hashlib.md5(f'{MINIO_ENDPOINT}'.encode()).hexdigest()
 
 
 class ClientTest(unittest.TestCase):
 
-    def test_publicServer(self):
-        MINIO_ENDPOINT = 'play.minio.io:9000'
-        MINIO_ACCESS_KEY = 'Q3AM3UQ867SPQQA43P2F'
-        MINIO_SECRET_KEY = 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
+    def test_list_buckets(self):
+        if c.isConnected():
+            self.assertTrue(c.list_buckets())
+            self.assertEqual(type(c.list_buckets()), list)
 
-        m = MinioClient(endpoint=MINIO_ENDPOINT, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET_KEY)
-        bucket = hashlib.md5(f'{MINIO_ENDPOINT}'.encode()).hexdigest()
+    def test_get_bucket(self):
+        if c.isConnected():
+            test = c.make_bucket(bucket)
 
-        if m.isConnected():
+            self.assertTrue(c.get_bucket(test))
+            self.assertTrue(type(c.get_bucket(test)), Bucket)
 
-            self.assertTrue(len(m.list_buckets()) >= 0)  # this might fail if no buckets
-            self.assertTrue(type(m.list_buckets()), list)
-            self.assertTrue(type(m.list_buckets(bucket)), list)
-
-            if m.list_buckets(bucket):
-                self.assertTrue(m.remove_bucket(bucket))
-            else:
-                self.assertTrue(m.make_bucket(bucket))
+    def test_remove_bucket(self):
+        if c.isConnected():
+            test = c.make_bucket(bucket)
+            self.assertTrue(c.remove_bucket(test))
 
 
 if __name__ == '__main__':
