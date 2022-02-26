111ff658c38a40e5820ed70314370de2ebdc96a4
naisanzaa <naisanza@gmail.com>
2022-02-26 17:30:43 -0500

    minio: config tests

--- automon/tests/integrations/minio/test_minio_config.py
+++ automon/tests/integrations/minio/test_minio_config.py
@@ -2,10 +2,16 @@ import unittest
 
 from automon.integrations.minio.config import MinioConfig
 
+c = MinioConfig()
+
 
 class ConfigTest(unittest.TestCase):
+
     def test_MinioConfig(self):
-        self.assertTrue(MinioConfig())
+        if c.isReady():
+            self.assertTrue(MinioConfig().isReady())
+        else:
+            self.assertFalse(MinioConfig().isReady())
 
 
 if __name__ == '__main__':
