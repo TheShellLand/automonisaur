b8c683b5b4a09ee1d8380ce51fd936374bd224c6
naisanzaa <naisanza@gmail.com>
2022-03-04 17:13:21 -0500

    0.2.13
    Change log:
    
    - hotfix: fix google client and config

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.2.12",
+    version="0.2.13",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
