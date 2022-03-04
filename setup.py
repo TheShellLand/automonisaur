bf4aa6bb26c23fde02fea4e3d0892c2cf3198a92
naisanzaa <naisanza@gmail.com>
2022-03-04 17:26:58 -0500

    0.2.14
    Change log:
    
    - hotfix: fix tests

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.2.13",
+    version="0.2.14",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
