1717a4153ef58ebfab106851711d675a059b99e6
naisanzaa <naisanza@gmail.com>
2022-03-09 03:17:50 -0500

    require pytest in setup

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TheShellLand/automonisaur",
     packages=setuptools.find_packages(),
+    tests_require=["pytest"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
