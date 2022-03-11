92ad19362a9eadea09140fd4c09e17fe9008a95c
naisanzaa <naisanza@gmail.com>
2022-03-10 19:29:47 -0500

    soar: Container just shows name

--- automon/integrations/splunk_soar/container.py
+++ automon/integrations/splunk_soar/container.py
@@ -12,21 +12,14 @@ class Container(Scaffolding):
     artifact_count: int
     start_time: datetime
     id: int
+    name: str
 
     def __init__(self, container: dict):
         self.artifact_count = None
         self.start_time = None
         self.id = None
+        self.name = None
         self.__dict__.update(container)
 
     def __repr__(self):
-        if len(self.to_json()) > 100:
-            return json.dumps(dict(
-                name=self.name,
-                label=self.label,
-                status=self.status,
-                id=self.id,
-                artifact_count=self.artifact_count,
-                create_time=self.create_time
-            ))
-        return self.to_json()
+        return self.name
