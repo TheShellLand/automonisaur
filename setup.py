699c6275cd275db9ccdd6d4ac299b9335c4d442b
naisanzaa <naisanza@gmail.com>
2022-03-19 16:32:33 -0700

    0.2.18
    Change log:
    
    - fix explicit imports
    - minor updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.2.17",
+    version="0.2.18",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
