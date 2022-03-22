f260c37ae89e53f1ab063380f74e7dd54beecc79
naisanzaa <naisanza@gmail.com>
2022-03-22 01:40:22 -0700

    soar: uat test

---
+++ automon/tests/integrations/splunk_soar/test_soar_uat.py
@@ -0,0 +1,57 @@
+import unittest
+
+from automon.integrations.splunk_soar import SplunkSoarClient
+
+c = SplunkSoarClient()
+
+
+class TestClient(unittest.TestCase):
+
+    def test_isConnected(self):
+        if c.isConnected():
+            self.assertTrue(c.isConnected())
+        else:
+            self.assertFalse(c.isConnected())
+
+    def test_create_artifact(self):
+        if c.isConnected():
+            id = c.create_container(label='testing', name='testing').id
+            self.assertTrue(c.create_artifact(container_id=id))
+        else:
+            self.assertFalse(c.create_artifact(container_id=0))
+
+    def test_create_container(self):
+        if c.isConnected():
+            self.assertTrue(c.create_container(label='testing', name='testing'))
+        else:
+            self.assertFalse(c.create_container(label='testing', name='testing'))
+
+    def test_delete_containers(self):
+        if c.isConnected():
+            container = c.create_container(label='testing', name='testing')
+            self.assertTrue(c.delete_container(container_id=container.id))
+        else:
+            self.assertFalse(c.delete_container(container_id=0))
+
+    def test_list_artifact(self):
+        if c.isConnected():
+            self.assertTrue(c.list_artifact())
+        else:
+            self.assertFalse(c.list_artifact())
+
+    def test_get_container(self):
+        if c.isConnected():
+            container = c.create_container(label='testing', name='testing')
+            self.assertTrue(c.get_container(container_id=container.id))
+        else:
+            self.assertFalse(c.get_container())
+
+    def test_list_containers(self):
+        if c.isConnected():
+            self.assertTrue(c.list_containers())
+        else:
+            self.assertFalse(c.list_containers())
+
+
+if __name__ == '__main__':
+    unittest.main()
