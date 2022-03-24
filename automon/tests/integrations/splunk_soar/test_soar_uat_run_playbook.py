b4f2b6aeda8ab7d388ad7ce7c9bae94f9748dfaa
naisanzaa <naisanza@gmail.com>
2022-03-24 02:21:11 -0700

    soar: add tests

--- automon/tests/integrations/splunk_soar/test_soar_uat_run_playbook.py
+++ automon/tests/integrations/splunk_soar/test_soar_uat_run_playbook.py
@@ -16,6 +16,17 @@ class TestClient(unittest.TestCase):
                 playbook_id=playbook
             )
 
+    def test_cancel_playbook(self):
+        if c.isConnected():
+            container = c.create_container(label='testing', name='testing')
+            playbook = ''
+            run = c.run_playbook(
+                container_id=container.id,
+                playbook_id=playbook
+            )
+            get = c.get_playbook_run(playbook_run_id=run.playbook_run_id)
+            cancel = c.cancel_playbook_run(run.playbook_run_id)
+
 
 if __name__ == '__main__':
     unittest.main()
