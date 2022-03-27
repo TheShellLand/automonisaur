650321a16934d054548d6c51dd151e4a37d33eec
naisanzaa <naisanza@gmail.com>
2022-03-26 19:34:10 -0700

    soar: tests

--- automon/tests/integrations/splunk_soar/test_soar_client.py
+++ automon/tests/integrations/splunk_soar/test_soar_client.py
@@ -6,51 +6,41 @@ c = SplunkSoarClient()
 
 
 class TestClient(unittest.TestCase):
-
-    def test_isConnected(self):
-        if c.isConnected():
+    if c.isConnected():
+        def test_isConnected(self):
             self.assertTrue(c.isConnected())
-        else:
-            self.assertFalse(c.isConnected())
 
-    def test_create_artifact(self):
-        if c.isConnected():
+        def test_create_artifact(self):
             id = c.create_container(label='testing', name='testing').id
             self.assertTrue(c.create_artifact(container_id=id))
-        else:
-            self.assertFalse(c.create_artifact(container_id=0))
 
-    def test_create_container(self):
-        if c.isConnected():
+        def test_create_container(self):
             self.assertTrue(c.create_container(label='testing', name='testing'))
-        else:
-            self.assertFalse(c.create_container(label='testing', name='testing'))
 
-    def test_delete_containers(self):
-        if c.isConnected():
+        def test_delete_containers(self):
             container = c.create_container(label='testing', name='testing')
             self.assertTrue(c.delete_container(container_id=container.id))
-        else:
-            self.assertFalse(c.delete_container(container_id=0))
 
-    def test_list_artifact(self):
-        if c.isConnected():
-            self.assertTrue(c.list_artifact())
-        else:
-            self.assertFalse(c.list_artifact())
-
-    def test_get_container(self):
-        if c.isConnected():
+        def test_get_container(self):
             container = c.create_container(label='testing', name='testing')
             self.assertTrue(c.get_container(container_id=container.id))
-        else:
-            self.assertFalse(c.get_container())
 
-    def test_list_containers(self):
-        if c.isConnected():
+        def test_list_artifact(self):
+            self.assertTrue(c.list_artifact())
+
+        def test_list_containers(self):
             self.assertTrue(c.list_containers())
-        else:
-            self.assertFalse(c.list_containers())
+
+        def test_list_vault(self):
+            self.assertTrue(c.list_vault())
+
+        def test_get_vault(self):
+            container = c.create_container(label='testing', name='testing')
+
+            list_vault = c.list_vault()
+            vault = list_vault.get_one()
+            if vault:
+                self.assertTrue(c.get_vault(vault_id=vault.id))
 
 
 if __name__ == '__main__':
