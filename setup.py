3d9a9c54d8fc28f5088f50f58d85042db64f5250
naisanzaa <naisanza@gmail.com>
2022-03-26 19:35:19 -0700

    0.2.20
    Change log:
    
    - soar: add vault
    - soar: add generic methods
    - soar: add tests

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.2.19",
+    version="0.2.20",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
