4fbd97bee445940985b4730707b51720ec30201f
naisanzaa <naisanza@gmail.com>
2022-03-28 14:48:22 -0700

    soar: add tests

---
+++ automon/tests/integrations/splunk_soar/test_soar_client_create_container_attachment.py
@@ -0,0 +1,27 @@
+import unittest
+
+from automon.integrations.splunk_soar import SplunkSoarClient
+
+c = SplunkSoarClient()
+
+
+class TestClient(unittest.TestCase):
+    if c.isConnected():
+        def test_soar_client_create_container_attachment(self):
+            container = c.create_container(label='testing', name='testing')
+            container = c.get_container(container_id=container.id)
+
+            test_file = open('dino.png', 'rb').read()
+
+            attachment = c.create_container_attachment(
+                container_id=container.id,
+                file_name='dino.png',
+                file_content=test_file,
+                metadata=None
+            )
+
+            self.assertTrue(attachment.vault_id)
+
+
+if __name__ == '__main__':
+    unittest.main()
