d9fb0bef332ad12dcdbffa0816058e78e98ee433
naisanzaa <naisanza@gmail.com>
2022-06-17 02:39:08 -0400

    instagram: add xpaths

---
+++ automon/integrations/instagram/xpaths.py
@@ -0,0 +1,29 @@
+class XPaths(object):
+    def __init__(self):
+        pass
+
+    @property
+    def login_user(self):
+        return '//*[@id="loginForm"]/div/div[1]/div/label/input'
+
+    @property
+    def login_pass(self):
+        return '//*[@id="loginForm"]/div/div[2]/div/label/input'
+
+    @property
+    def login_button(self):
+        return '//*[@id="loginForm"]/div/div[3]/button'
+
+    @property
+    def login_pass_xpaths(self):
+        return [
+            '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input',
+            '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/div/label/input'
+        ]
+
+    @property
+    def login_btn_xpaths(self):
+        return [
+            '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button',
+            '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[6]/button'
+        ]
