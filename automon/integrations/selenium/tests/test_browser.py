16083bc4efb0578db4b171d828e28cd5ac8068aa
naisanzaa <naisanza@gmail.com>
2022-08-10 01:36:24 -0700

    selenium: fix tests

--- automon/integrations/selenium/tests/test_browser.py
+++ automon/integrations/selenium/tests/test_browser.py
@@ -3,25 +3,27 @@ import unittest
 from automon.integrations.selenium.browser import SeleniumBrowser
 
 browser = SeleniumBrowser()
-browser.set_browser(browser.type.chrome)
+browser.set_driver(browser.type.chrome)
 
 
 class SeleniumClientTest(unittest.TestCase):
     if browser.isRunning():
-        def test_get(self):
+        def test(self):
             self.assertFalse(browser.get('http://555.555.555.555'))
             if browser.get('http://1.1.1.1'):
                 self.assertTrue(True)
 
-        def test_get_screenshot_as_png(self):
-            self.assertTrue(browser.get_screenshot_as_png())
+            if browser.get('http://google.com'):
+                self.assertTrue(browser.get_screenshot_as_png())
 
-        def test_get_screenshot_as_base64(self):
-            self.assertTrue(browser.get_screenshot_as_base64())
+            if browser.get('http://yahoo.com'):
+                self.assertTrue(browser.get_screenshot_as_base64())
 
-        def test_save_screenshot(self):
-            self.assertTrue(browser.save_screenshot())
-            self.assertTrue(browser.save_screenshot(folder='./'))
+            if browser.get('http://bing.com'):
+                self.assertTrue(browser.save_screenshot())
+                self.assertTrue(browser.save_screenshot(folder='./'))
+
+            browser.quit()
 
 
 if __name__ == '__main__':
