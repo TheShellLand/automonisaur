14cbbbab131cec9de642f34817678a16be36dcc0
naisanzaa <naisanza@gmail.com>
2022-10-04 15:37:58 -0700

    soar: update tests

--- automon/integrations/splunk_soar/tests/test_soar_client.py
+++ automon/integrations/splunk_soar/tests/test_soar_client.py
@@ -7,9 +7,6 @@ c = SplunkSoarClient()
 
 class TestClient(unittest.TestCase):
     if c.isConnected():
-        def test_isConnected(self):
-            self.assertTrue(c.isConnected())
-
         def test_create_artifact(self):
             id = c.create_container(label='testing', name='testing').id
             self.assertTrue(c.create_artifact(container_id=id))
@@ -25,6 +22,17 @@ class TestClient(unittest.TestCase):
             container = c.create_container(label='testing', name='testing')
             self.assertTrue(c.get_container(container_id=container.id))
 
+        def test_get_vault(self):
+            container = c.create_container(label='testing', name='testing')
+
+            list_vault = c.list_vault()
+            vault = list_vault.get_one()
+            if vault:
+                self.assertTrue(c.get_vault(vault_id=vault.id))
+
+        def test_isConnected(self):
+            self.assertTrue(c.isConnected())
+
         def test_list_artifact(self):
             self.assertTrue(c.list_artifact())
 
@@ -33,14 +41,6 @@ class TestClient(unittest.TestCase):
 
         def test_list_vault(self):
             self.assertTrue(c.list_vault())
-
-        def test_get_vault(self):
-            container = c.create_container(label='testing', name='testing')
-
-            list_vault = c.list_vault()
-            vault = list_vault.get_one()
-            if vault:
-                self.assertTrue(c.get_vault(vault_id=vault.id))
 
 
 if __name__ == '__main__':
