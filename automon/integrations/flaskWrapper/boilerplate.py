aa9f7915419306c548bd31f7c4d951bf25c6d778
naisanzaa <naisanza@gmail.com>
2022-10-18 13:45:11 -0700

    flask: enable_javascript_compatibility as a method

--- automon/integrations/flaskWrapper/boilerplate.py
+++ automon/integrations/flaskWrapper/boilerplate.py
@@ -7,21 +7,24 @@ from automon.integrations.flaskWrapper.config import FlaskConfig
 
 class FlaskBoilerplate(object):
 
-    def __init__(self, import_name: str = __name__, config: FlaskConfig = None,
-                 enable_javascript_compatibility: bool = False, **kwargs):
+    def __init__(self, import_name: str = __name__, config: FlaskConfig = None, **kwargs):
         """Wrapper for flask"""
         self._log = Logging(FlaskBoilerplate.__name__, Logging.DEBUG)
 
         self.Flask = Flask(import_name=import_name, **kwargs)
         self.config = config or FlaskConfig()
 
-        if enable_javascript_compatibility:
-            self.Flask = FlaskConfig.javascript_compatibility(self.Flask)
+    def __repr__(self):
+        return f'{self.Flask}'
 
     @property
     def request(self):
         """Get flask request"""
         return flask.request
+
+    def enable_javascript_compatibility(self):
+        """Enable Jinya compatibility for JavaScript"""
+        self.Flask = FlaskConfig.javascript_compatibility(self.Flask)
 
     def run(self, port: int = None, debug: bool = False, **kwargs):
         """Run flask app"""
