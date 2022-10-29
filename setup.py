f227edbe9ce0a7a4246fb85badd0a623b88fb5c8
naisanzaa <naisanza@gmail.com>
2022-10-29 16:48:06 -0700

    0.2.35
    Change log:
    
    snmp: fix imports
    slack: fix import loop
    update install-requirements.sh

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.2.34",
+    version="0.2.35",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
