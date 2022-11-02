575e703d7d335b3ef8509825b927265b44f31481
naisanzaa <naisanza@gmail.com>
2022-11-02 02:50:58 -0700

    youuuuuuutubedl: fix thread_pool minimum size

--- automon/integrations/youuuuuuutubedl/thread.py
+++ automon/integrations/youuuuuuutubedl/thread.py
@@ -4,7 +4,11 @@ from concurrent.futures import (ThreadPoolExecutor, wait, as_completed)
 class ThreadPool:
 
     @staticmethod
-    def config_thread_pool(thread_pool: int) -> ThreadPoolExecutor:
+    def config_thread_pool(thread_pool: int = 1) -> ThreadPoolExecutor:
         """Configure threading pool
+
+        thread_pool: must be > 0
         """
+        if not thread_pool:
+            thread_pool = 1
         return ThreadPoolExecutor(max_workers=thread_pool)
