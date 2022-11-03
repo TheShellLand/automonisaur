3c5872549e1078909376ca35067cfc42f490e7f4
naisanzaa <naisanza@gmail.com>
2022-11-03 16:17:57 -0700

    requests: update config

--- automon/integrations/requestsWrapper/config.py
+++ automon/integrations/requestsWrapper/config.py
@@ -6,5 +6,12 @@ log = Logging(name='RequestsConfig', level=Logging.DEBUG)
 
 
 class RequestsConfig(object):
+
+    def __init__(self):
+        pass
+
+    def isReady(self):
+        return f'{NotImplemented}'
+
     def __repr__(self):
         return f'{NotImplemented}'
