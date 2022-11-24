3acf2a52771983d8a245c5b9ddef13edd0c8568a
naisanzaa <naisanza@gmail.com>
2022-11-24 16:28:15 +0700

    0.2.42
    Change log:
    
    run: fix var ref bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.2.41",
+    version="0.2.42",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
