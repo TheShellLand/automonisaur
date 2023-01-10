db6a0b3ad4222022c0731920c8c8e6582cb76b3d
naisanzaa <naisanza@gmail.com>
2023-01-11 05:27:34 +0700

    instagram: update tests

--- automon/integrations/instagram/tests/test_instagram_browser.py
+++ automon/integrations/instagram/tests/test_instagram_browser.py
@@ -6,10 +6,11 @@ c = InstagramBrowserClient(headless=False)
 
 
 class InstagramClientTest(unittest.TestCase):
-    if c.authenticate():
-        def test_authenticate(self):
-            self.assertTrue(c.is_authenticated())
-            c.browser.quit()
+    if c.is_running():
+        if c.authenticate():
+            def test_authenticate(self):
+                self.assertTrue(c.is_authenticated())
+                c.browser.quit()
 
 
 if __name__ == '__main__':
