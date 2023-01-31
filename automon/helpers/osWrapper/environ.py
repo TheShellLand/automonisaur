4248a03c804df6f499b7a885b332465c87c445e7
naisanzaa <naisanza@gmail.com>
2023-01-31 16:02:21 +0700

    os: update docstring

--- automon/helpers/osWrapper/environ.py
+++ automon/helpers/osWrapper/environ.py
@@ -2,6 +2,7 @@ import os
 
 
 def environ(env_var: str, default: any = None) -> bool or str or None:
+    """Get environment variable, else return default"""
     env = os.getenv(env_var)
     if env:
         if f'{env}'.lower() == 'true':
