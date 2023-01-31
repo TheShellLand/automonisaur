9f4936203dd2e4b736b60db6fbf37caf01d664a1
naisanzaa <naisanza@gmail.com>
2023-01-31 21:37:32 +0700

    minio: fix tests

--- automon/integrations/minioWrapper/tests/test_minio_client_public.py
+++ automon/integrations/minioWrapper/tests/test_minio_client_public.py
@@ -23,8 +23,8 @@ class ClientTest(unittest.TestCase):
         if client.is_connected():
             test = client.make_bucket(bucket)
 
-            self.assertTrue(client.get_bucket(test))
-            self.assertTrue(type(client.get_bucket(test)), Bucket)
+            self.assertTrue(client.get_bucket(test.name))
+            self.assertTrue(type(client.get_bucket(test.name)), Bucket)
 
 
 if __name__ == '__main__':
