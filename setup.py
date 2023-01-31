618188078fe4f2d2c3705e649ff897bc2b59da11
naisanzaa <naisanza@gmail.com>
2023-01-31 21:59:25 +0700

    0.2.48
    Change log:
    
    minio: fix tests
    minio: update tests
    minio: update client docstrings, fix unexpected returns
    minio: update Bucket repr and str
    minio: update human readable size
    minio: fix list_objects expected return
    minio: update tests
    minio: refactor list_objects to include more kwargs
    minio: type hints, refactor `is_ready` to property
    minio: PEP 8
    
    os: update docstring
    os: type hints
    
    math: human readable size formatting
    neo4j: add `is_ready` property
    
    soar: refactor `is_ready` to property
    soar: PEP 8
    
    requests: PEP 8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.2.47",
+    version="0.2.48",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
