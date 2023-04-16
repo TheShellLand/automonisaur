b5b483a224c388efb8245d60a522889c5be2dedc
naisanzaa <naisanza@gmail.com>
2023-04-17 05:30:28 +0800

    seleniumWrapper: add user agents

---
+++ automon/integrations/seleniumWrapper/tests/test_user_agent.py
@@ -0,0 +1,26 @@
+import unittest
+
+from automon.integrations.seleniumWrapper.user_agents import SeleniumUserAgentBuilder
+
+
+class MyTestCase(unittest.TestCase):
+    def test_filter(self):
+        test = SeleniumUserAgentBuilder()
+        self.assertTrue(test.filter_agent('applewebkit'))
+        self.assertTrue(test.filter_agent('AppleWebKit', case_sensitive=True))
+
+        self.assertFalse(test.filter_agent('xxxxx'))
+        self.assertFalse(test.filter_agent('xxxxx', case_sensitive=True))
+
+
+    def test_random(self):
+        test = SeleniumUserAgentBuilder()
+        self.assertTrue(test.get_random('applewebkit'))
+        self.assertTrue(test.get_random('AppleWebKit', case_sensitive=True))
+
+        self.assertFalse(test.get_random('xxxxx'))
+        self.assertFalse(test.get_random('xxxxx', case_sensitive=True))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ automon/integrations/seleniumWrapper/user_agents.py
@@ -0,0 +1,48 @@
+import random
+
+from automon import Logging
+
+log = Logging(name='SeleniumUserAgentBuilder', level=Logging.DEBUG)
+
+
+class SeleniumUserAgentBuilder:
+    agents = [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_3_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 13.3; rv:112.0) Gecko/20100101 Firefox/112.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_3_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_3_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Vivaldi/5.7.2921.68',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_3_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.48',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.48',
+        'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:112.0) Gecko/20100101 Firefox/112.0',
+        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Vivaldi/5.7.2921.68',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Vivaldi/5.7.2921.68',
+    ]
+
+    def filter_agent(self, filter: list or str, case_sensitive: bool = False) -> list:
+        if isinstance(filter, str):
+            filter = [filter]
+
+        filtered_agents = []
+        for agent in self.agents:
+            if not case_sensitive:
+                if any(substring.lower() in agent.lower() for substring in filter):
+                    filtered_agents.append(agent)
+            else:
+                if any(substring in agent for substring in filter):
+                    filtered_agents.append(agent)
+
+        return filtered_agents
+
+    def get_random(self, filter: list = None, **kwargs):
+        if filter:
+            filtered_agents = self.filter_agent(filter, **kwargs)
+            if filtered_agents:
+                return random.choice(filtered_agents)
+            else:
+                return ''
+
+        return random.choice(self.agents)
