11412c61a2742a71cd146eff377d416aaab36a41
naisanzaa <naisanza@gmail.com>
2023-04-19 04:49:50 +0800

    instagram: urls for followers and following

--- automon/integrations/instagram/urls.py
+++ automon/integrations/instagram/urls.py
@@ -6,3 +6,11 @@ class Urls(object):
     @property
     def login_page(self):
         return 'https://www.instagram.com/accounts/login/?source=auth_switcher'
+
+    @staticmethod
+    def followers(account: str):
+        return f'https://www.instagram.com/{account}/followers/'
+
+    @staticmethod
+    def following(account: str):
+        return f'https://www.instagram.com/{account}/following/'
