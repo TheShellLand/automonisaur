6adea05f44bab58dc81bbfeff987535c35f6bd25
naisanzaa <naisanza@gmail.com>
2023-04-22 02:44:41 +0800

    selenium: add authenticated xpaths

--- automon/integrations/instagram/xpaths.py
+++ automon/integrations/instagram/xpaths.py
@@ -26,6 +26,19 @@ class XPaths(object):
         ]
 
     @property
+    def authenticated_paths(self):
+        authenticated = []
+        authenticated.extend(self.profile_picture)
+        authenticated.extend(self.home)
+        return authenticated
+
+    @property
+    def home(self):
+        return [
+            '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[1]/div/div/a/div',
+        ]
+
+    @property
     def profile_picture(self):
         return [
             '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div[1]/section/div[3]/div[1]/div/div/div/div/div/div[1]/div/div/span/img',
