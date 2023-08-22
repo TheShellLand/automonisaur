1614b5e40310eae9ef2c378ecb5a03b1ede18707
naisanzaa <naisanza@gmail.com>
2023-08-22 18:23:05 -0400

    0.3.3
    Change log:
    
    scrapy: fix for python36

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.3.2",
+    version="0.3.3",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
