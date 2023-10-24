e5a55265873d1278592fa872a6e4788b75290b67
naisanzaa <naisanza@gmail.com>
2023-10-23 20:58:13 -0500

    selenium: fix tests to be headless

--- automon/integrations/seleniumWrapper/tests/test_browser_useragent.py
+++ automon/integrations/seleniumWrapper/tests/test_browser_useragent.py
@@ -3,7 +3,7 @@ import unittest
 from automon.integrations.seleniumWrapper.browser import SeleniumBrowser
 
 browser = SeleniumBrowser()
-browser.config.set_webdriver.Chrome().enable_defaults()
+browser.config.set_webdriver.Chrome().enable_defaults().enable_headless()
 
 agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:112.0) Gecko/20100101 Firefox/112.0'
 
