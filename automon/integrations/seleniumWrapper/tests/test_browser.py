bc494d01fce5fc49d005f3625867bdce11a74959
naisanzaa <naisanza@gmail.com>
2023-10-24 04:59:33 -0500

    selenium: fix tests

--- automon/integrations/seleniumWrapper/tests/test_browser.py
+++ automon/integrations/seleniumWrapper/tests/test_browser.py
@@ -3,28 +3,32 @@ import unittest
 from automon.integrations.seleniumWrapper.browser import SeleniumBrowser
 
 browser = SeleniumBrowser()
-browser.config.set_webdriver.Chrome().enable_defaults()
+browser.config.set_webdriver.Chrome().enable_defaults().enable_headless()
 
 
 class SeleniumClientTest(unittest.TestCase):
     if browser.run():
-        def test(self):
+        def test_fake_page(self):
             self.assertFalse(browser.get('http://555.555.555.555'))
+
+        def test_real_page(self):
             if browser.get('http://1.1.1.1'):
                 self.assertTrue(True)
 
+        def test_screenshot_png(self):
             if browser.get('http://google.com'):
                 self.assertTrue(browser.get_screenshot_as_png())
 
+        def test_screenshot_base64(self):
             if browser.get('http://yahoo.com'):
                 self.assertTrue(browser.get_screenshot_as_base64())
 
+        def test_screenshot_file(self):
             if browser.get('http://bing.com'):
                 self.assertTrue(browser.save_screenshot())
                 self.assertTrue(browser.save_screenshot(folder='./'))
 
-            browser.quit()
-
 
 if __name__ == '__main__':
     unittest.main()
+    browser.quit()
