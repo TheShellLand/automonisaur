9e2ee8d07f6a9d4efa4a06ff18b826ba91ff07a6
naisanzaa <naisanza@gmail.com>
2023-10-27 16:38:58 -0500

    0.3.10
    Change log:
    
    selenium: fix wait_for method logger
    selenium: fix get() logger. add request_status property. remove status property.
    
    facebook: fix error logger

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.3.9",
+    version="0.3.10",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
