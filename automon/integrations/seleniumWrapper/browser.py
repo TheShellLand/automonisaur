cf786f66e42670ac569e5342c8528702ec20b048
naisanzaa <naisanza@gmail.com>
2023-10-28 04:45:19 -0500

    selenium: default action_type secret to True

--- automon/integrations/seleniumWrapper/browser.py
+++ automon/integrations/seleniumWrapper/browser.py
@@ -156,7 +156,7 @@ class SeleniumBrowser(object):
         return False
 
     @_is_running
-    def action_type(self, key: str or Keys, secret: bool = False):
+    def action_type(self, key: str or Keys, secret: bool = True):
         """perform keyboard command"""
         try:
             actions = selenium.webdriver.common.action_chains.ActionChains(
