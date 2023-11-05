9218722d287ee62ed5fd6c9dc8c11b8cabbae8ac
naisanzaa <naisanza@gmail.com>
2023-11-05 04:25:15 -0800

    0.3.13
    Change log:
    
    facebook: support setting user agent

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.3.12",
+    version="0.3.13",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
