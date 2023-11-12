a5c9dbd609c27281d6d2bb16b84350298f763017
naisanzaa <naisanza@gmail.com>
2023-11-11 21:32:25 -0800

    0.3.15
    Change log:
    
    selenium: fix current_url, update get, remove is_running
    selenium: add get_screenshot_as_file
    selenium: update config
    selenium: update logger
    
    facebook: add is_temporarily_blocked
    facebook: update with rate limiting
    
    sleeper: fix logger

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.3.14",
+    version="0.3.15",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
