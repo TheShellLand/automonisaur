0f516cc800b8d5db6ed4351acd6fb89cc0e5b1bb
naisanzaa <naisanza@gmail.com>
2023-11-13 21:20:39 -0800

    0.3.17
    Change log:
    
    facebook: add global rate limiter
    facebook: set result to False if rate limited
    facebook: add debugging
    facebook: add must_login. update rate_limited
    
    sleeper: simplify args

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.3.16",
+    version="0.3.17",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
