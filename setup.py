c6e97b057b8bb0207068aef2c5ce1030fb5e1e0b
naisanzaa <naisanza@gmail.com>
2023-11-21 16:32:07 -0800

    0.3.22
    Change log:
    
    selenium: add current_url
    facebook: fix rate limiter

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.3.21",
+    version="0.3.22",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
