73568ee597d2f2971b5bc0fc73d7814b5a69fa43
naisanzaa <naisanza@gmail.com>
2023-11-27 22:39:01 -0800

    0.3.26
    Change log:
    
    selenium: add more cookie handling
    selenium: update user agents
    selenium: add user_agent property

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.3.25",
+    version="0.3.26",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
