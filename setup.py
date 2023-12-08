3f910934fa5f18a8fac9bdb9eb117dce942a362b
naisanzaa <naisanza@gmail.com>
2023-12-08 17:50:16 +0800

    0.3.27
    Change log:
    
    facebook: update logging
    facebook: update members_count
    facebook: fix method calls
    
    airport: update ssid summary logging
    airport: fix create_psk improper string
    
    github actions: fix scripts

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.3.26",
+    version="0.3.27",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
