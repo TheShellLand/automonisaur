cd1749f068db3a9dc82f7601ba89b043aaa6f616
naisanzaa <naisanza@gmail.com>
2023-12-27 11:22:20 +0800

    logger: update default logging format to name_and_lineno

--- automon/log/__init__.py
+++ automon/log/__init__.py
@@ -2,5 +2,5 @@ from .attributes import LogRecordAttribute
 from .logger import Logging, LogStream, TEST, DEBUG, INFO, WARN, ERROR, CRITICAL, NOTSET
 from .logger import logging
 
-log_format = f'{LogRecordAttribute(timestamp=True).levelname().name().funcName().message()}'
+log_format = f'{LogRecordAttribute(timestamp=True).levelname().name_and_lineno().funcName().message()}'
 logging.basicConfig(level=DEBUG, format=log_format)
