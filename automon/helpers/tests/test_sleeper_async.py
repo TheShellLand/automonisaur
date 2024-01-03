5e4d186ab60c399fea49e5fd32ec4665aa07ca9b
naisanzaa <naisanza@gmail.com>
2024-01-04 04:21:11 +0900

    sleeper: fix test

--- automon/helpers/tests/test_sleeper_async.py
+++ automon/helpers/tests/test_sleeper_async.py
@@ -6,7 +6,7 @@ from automon.helpers.sleeper import Sleeper
 
 class SleeperTest(unittest.TestCase):
     def test_Sleeper(self):
-        loop = asyncio.get_event_loop()
+        loop = asyncio.new_event_loop()
         task = loop.run_until_complete(Sleeper.seconds_async(1))
 
 
