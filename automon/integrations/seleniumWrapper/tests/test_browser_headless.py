363a6135118a4a4adab4873bdfa272f789ae66b2
naisanzaa <naisanza@gmail.com>
2024-03-11 23:13:30 -0700

    selenium: fix test_browser_headless.py

--- automon/integrations/seleniumWrapper/tests/test_browser_headless.py
+++ automon/integrations/seleniumWrapper/tests/test_browser_headless.py
@@ -16,19 +16,12 @@ class SeleniumClientTest(unittest.TestCase):
         if asyncio.run(browser.run()):
             asyncio.run(browser.set_window_size(device_type='web-large'))
 
-        while True:
+            if asyncio.run(browser.get('http://bing.com')):
+                self.assertTrue(asyncio.run(browser.save_screenshot()))
+                self.assertTrue(asyncio.run(browser.save_screenshot()))
+                self.assertTrue(asyncio.run(browser.save_screenshot(folder='./')))
 
-            try:
-                if asyncio.run(browser.get('http://bing.com')):
-                    self.assertTrue(asyncio.run(browser.save_screenshot()))
-                    self.assertTrue(asyncio.run(browser.save_screenshot()))
-                    self.assertTrue(asyncio.run(browser.save_screenshot(folder='./')))
-
-                asyncio.run(browser.quit())
-                break
-
-            except:
-                pass
+            asyncio.run(browser.quit())
 
 
 if __name__ == '__main__':
