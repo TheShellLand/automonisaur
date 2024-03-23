c1d5c6bcadbb2e92b1073aca9d2c22b1a142fbb2
laerfulaolun <laerfulaolun@gmail.com>
2024-03-22 21:50:32 -0700

    swimlane: fix tests

--- automon/integrations/swimlaneWrapper/tests/test_rest_record.py
+++ automon/integrations/swimlaneWrapper/tests/test_rest_record.py
@@ -10,13 +10,9 @@ class MyTestCase(unittest.TestCase):
     def test_login(self):
         if asyncio.run(client.is_ready()):
             if asyncio.run(client.login()):
-                self.assertTrue(asyncio.run(
-                    client.app_list())
-                )
-
                 app_id = client.config.appId
                 record = asyncio.run(
-                    client.record_list(appId=app_id)
+                    client.record_schema(appId=app_id)
                 )
 
                 self.assertTrue(record)
