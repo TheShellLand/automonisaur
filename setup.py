c35bad145f44e6b20ccda18a2a64067cc7432300
naisanzaa <naisanza@gmail.com>
2024-04-02 17:30:27 -0400

    0.5.0
    Change log:
    
    add opentelemetry
    add datadog
    add swimlane
    
    fix bugs
    fix imports
    
    update requests to async
    
    opentelemetry: add consumer and producer
    opentelemetry: add pop_finished_spans
    opentelemetry: fix test
    opentelemetry: fix to_dict. add to_datadog.
    opentelemetry: add working in-memory tracer
    opentelemetry: update requirements.txt
    opentelemetry: fix for 0.44b0
    opentelemetry: update docker
    opentelemetry: update readme
    opentelemetry: fix python packages
    
    datadog: update logging
    datadog: fix log
    datadog: add client
    datadog: add rest client
    
    selenium: fix test_browser_headless.py
    selenium: update driver to 122.0.6261.111
    selenium: fix tests
    selenium: fix error message
    selenium: raise exception if webdriver is missing before using it
    
    swimlane: add logging_by_id. add logging_recent.
    swimlane: add app_by_id. add app_export
    swimlane: add tests
    swimlane: add tests
    swimlane: fix tests
    swimlane: add tests
    swimlane: add method record_resolve_fields. rename method record_schema. fix method record_create_hard
    swimlane: update test
    swimlane: disable library test. its dependencies are so old it breaks python > 3.10
    swimlane: add python package
    swimlane: fix record hardcoded key hash
    swimlane: fix test_library_record_create.py
    swimlane: update test_rest_record.py
    swimlane: update test_rest_app.py
    swimlane: return record after created
    swimlane: add test_rest_record_create.py
    swimlane: add test_rest_auth_token.py
    swimlane: fix credentials
    swimlane: update record_create_easy
    swimlane: fix login_token
    swimlane: v2 add user authorize endpoint
    swimlane: add appId to config
    swimlane: add get record
    swimlane: fix record methods
    swimlane: rename tests
    swimlane: add test_library_record_create.py
    swimlane: update client
    swimlane: add headers_jwt_token
    swimlane: update record methods
    swimlane: support api "record"
    swimlane: support apis "app", "workspace"
    swimlane: update client and config
    swimlane: add client and config
    
    requests: fix tests for async
    requests: fix rest to async
    requests: fix config
    requests: add content_to_dict
    requests: fix to_dict, fix to_json
    requests: rename property results to response
    requests: default to requests.Session
    requests: minor support for request.Session
    requests: add content prop, add to_json method
    requests: add reason method
    requests: update to async
    requests: add content_to_dict
    requests: fix to_dict, fix to_json
    requests: rename property results to response
    requests: default to requests.Session
    requests: minor support for request.Session
    requests: add content prop, add to_json method
    requests: add reason method
    requests: update to async
    
    automon: update readme
    automon: fix readme
    automon: fix test.sh
    automon: fix missing deprecation package
    
    pypi: quotes around vars
    pypi: quotes around vars
    
    psutil: move to integrations
    nestasyncio: move to integrations
    helpers: import networking
    robinhood: add status api endpoints
    
    grok: move to integrations
    grok: removing depreciated code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.4.2",
+    version="0.5.0",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
