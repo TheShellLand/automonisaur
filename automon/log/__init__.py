c48e413b6c2ca28abb5095a46cd37c7b2b6eec80
naisanzaa <naisanza@gmail.com>
2024-04-15 04:10:44 -0400

    logger: log format string defaults to env var OTEL_PYTHON_LOG_FORMAT

--- automon/log/__init__.py
+++ automon/log/__init__.py
@@ -1,11 +1,13 @@
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
 
+from automon.helpers.osWrapper import environ
+
 from .attributes import LogRecordAttribute
 from .logger import Logging, LogStream, TEST, DEBUG, INFO, WARN, ERROR, CRITICAL, NOTSET
 from .logger import logging
 
 log_format = f'{LogRecordAttribute(timestamp=True).levelname().name_and_lineno().funcName().message()}'
-log_format_opentelemetry = '\t'.join(
+log_format_opentelemetry = environ('OTEL_PYTHON_LOG_FORMAT') or '\t'.join(
     [
         f'%(asctime)s',
         f'%(levelname)s',
