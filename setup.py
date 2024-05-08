0517a704045d6c8944f3e46b87ee24e4e30a620f
naisanzaa <naisanza@gmail.com>
2024-05-08 17:50:00 +0800

    0.5.2
    Change log:
    
    selenium: use sync for configuring chrome driver
    selenium: fix error logging
    selenium: fix tests
    
    opentelemetry: update env vars
    opentelemetry: update to async
    opentelemetry: add instrumentation log format
    opentelemetry: add event. add exception. add contextmanager from client.  update datadog
    
    datadog: must use self.tracer.start_as_current_span
    datadog: must use self.config.opentelemetry.trace.get_current_span().add_event
    datadog: add opentelemetry exporter
    datadog: required packages
    
    swimlane: update tests
    swimlane: logging_recent api is convoluted
    swimlane: update env vars
    
    logger: fix import
    logger: log format string defaults to env var OTEL_PYTHON_LOG_FORMAT
    logging: fix opentelemetry instrumentation
    logging: add opentelemetry insrumentation
    
    github actions: disable python39.yml
    github actions: disable python38.yml
    github actions: disable python37.yml
    github actions: disable python36.yml
    
    automon: require python '>=3.10'
    ci/cd: update chromedriver 124.0.6367.91
    sentryio: fix depreciated args
    instagram: fix ChromeWrapper
    neo4j: fix depreciated import
    airport: fix missing async await

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.5.1",
+    version="0.5.2",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
