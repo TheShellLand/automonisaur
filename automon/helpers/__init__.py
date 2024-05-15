6208f6c887754d3a12bbc74de2589869f7419b6e
naisanzaa <naisanza@gmail.com>
2024-05-16 00:27:48 +0800

    helpers: refactor

--- automon/helpers/__init__.py
+++ automon/helpers/__init__.py
@@ -1,6 +1,6 @@
 from .dates import Dates
 from .markdown import Chat, Format
 from .networking import Networking
-from .osWrapper import environ
+from .osWrapper import environ, environ_list
 from .sleeper import Sleeper
 from .subprocessWrapper import Run
