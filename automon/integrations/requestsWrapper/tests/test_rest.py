fed29c1caf92d45804480070bac3b66e18a451b8
naisanzaa <naisanza@gmail.com>
2024-06-23 18:55:30 -0700

    requests: fix tests

--- automon/integrations/requestsWrapper/tests/test_rest.py
+++ automon/integrations/requestsWrapper/tests/test_rest.py
@@ -9,7 +9,7 @@ r = BaseRestClient()
 class Client(unittest.TestCase):
     def test_get(self):
         self.assertTrue(asyncio.run(r.get('https://1.1.1.1')))
-        self.assertTrue(r.requests.get('https://1.1.1.1'))
+        self.assertTrue(asyncio.run(r.requests.get('https://1.1.1.1')))
         self.assertFalse(asyncio.run(r.get('x://127.0.0.1')))
 
 
--- automon/integrations/requestsWrapper/tests/test_rest_inherit.py
+++ automon/integrations/requestsWrapper/tests/test_rest_inherit.py
@@ -4,16 +4,16 @@ import unittest
 from automon.integrations.requestsWrapper.rest import BaseRestClient
 
 
-class Test(BaseRestClient):
+class Inherit(BaseRestClient):
 
     def __init__(self):
-        BaseRestClient.__init__(self)
+        super().__init__()
         pass
 
 
 class Client(unittest.TestCase):
     def test_get(self):
-        self.assertTrue(asyncio.run(Test().get(url='https://1.1.1.1')))
+        self.assertTrue(asyncio.run(Inherit().get(url='https://1.1.1.1')))
 
 
 if __name__ == '__main__':
