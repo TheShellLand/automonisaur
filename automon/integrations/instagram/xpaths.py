b6fd055b666815a272dafe30e4510a2c68472400
naisanzaa <naisanza@gmail.com>
2024-07-09 04:20:54 -0700

    instagram: fix xpaths

--- automon/integrations/instagram/xpaths.py
+++ automon/integrations/instagram/xpaths.py
@@ -6,26 +6,19 @@ class XPaths(object):
     @property
     def login_user(self):
         return [
-            '/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[1]/div/label/input',
-            '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[1]/div/label/input',
-            '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[1]/div/label/input',
+            '//*[@id="loginForm"]/div/div[1]/div/label/input'
         ]
 
     @property
     def login_pass(self):
         return [
-            '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[2]/div/label/input',
-            '/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[2]/div/label/input',
-            '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[2]/div/label/input'
+            '//*[@id="loginForm"]/div/div[2]/div/label/input'
         ]
 
     @property
     def login_btn(self):
         return [
-            '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[3]/button',
-            '/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[3]/button',
-            '/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[4]/button',
-            '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div/div[3]/button'
+            '//*[@id="loginForm"]/div/div[3]/button'
         ]
 
     @property
