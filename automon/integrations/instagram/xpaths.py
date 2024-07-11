2368ac4a28a246ac2eb82a88bd26f9d887c82dcf
naisanzaa <naisanza@gmail.com>
2024-07-11 03:05:39 -0700

    instagram: cleanup

--- automon/integrations/instagram/xpaths.py
+++ automon/integrations/instagram/xpaths.py
@@ -4,24 +4,6 @@ class XPaths(object):
         return 'Instagram XPaths'
 
     @property
-    def login_user(self):
-        return [
-            '//*[@id="loginForm"]/div/div[1]/div/label/input'
-        ]
-
-    @property
-    def login_pass(self):
-        return [
-            '//*[@id="loginForm"]/div/div[2]/div/label/input'
-        ]
-
-    @property
-    def login_btn(self):
-        return [
-            '//*[@id="loginForm"]/div/div[3]/button'
-        ]
-
-    @property
     def authenticated_paths(self):
         authenticated = []
         authenticated.extend(self.profile_picture)
@@ -32,47 +14,4 @@ class XPaths(object):
     def home(self):
         return [
             '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[1]/div/div/a/div',
-        ]
-
-    @property
-    def profile_picture(self):
-        return [
-            '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div[1]/div[2]/div/div[1]/div/div/div/div/div/div[1]/div/div/a/img',
-            '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[8]/div/span/div/a/div/div[1]/div/div/span/img',
-            '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div/div[6]/div/span/div/a/div/div/div/div/span/img',
-            '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div[1]/section/div[3]/div[1]/div/div/div/div/div/div[1]/div/div/span/img',
-            '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div[1]/section/div/div[2]/div/div/div/div/ul/li[3]/div/button/div[1]/span/img',
-        ]
-
-    @property
-    def save_info(self):
-        return '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div/section/div/button'
-
-    @property
-    def save_info_not_now_div(self):
-        return [
-            '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div',
-        ]
-
-    @property
-    def save_info_not_now(self):
-        return [
-            '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]',
-            '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div',
-            '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/div/div/div/div/button'
-        ]
-
-    @property
-    def turn_on_notifications(self):
-        return [
-            '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[1]',
-            '/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[1]'
-        ]
-
-    @property
-    def turn_on_notifications_not_now(self):
-        return [
-            '/html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]',
-            '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]',
-            '/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]'
         ]
