d0c66c00d247b6c230dca3e6cf8371b35732c64d
naisanzaa <naisanza@gmail.com>
2024-09-26 03:59:06 +0800

    cicd: minor update install-local.sh

--- install-local.sh
+++ install-local.sh
@@ -7,6 +7,9 @@ cd "$(dirname $0)"; set -xe
 python3 -m pip uninstall automonisaur -y
 python3 -m pip install --use-pep517 -e ./
 
+set +x
+
 if [ ! -z "$@" ]; then
+  set -x
   python3 -m pip install -U "$@"
 fi
