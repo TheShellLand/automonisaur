ee07ed22290282f95a9918ff2b1707334505ed6f
naisanzaa <naisanza@gmail.com>
2024-09-26 03:58:34 +0800

    cicd: use --use-pep517 to allow local editable install

--- install-local.sh
+++ install-local.sh
@@ -5,7 +5,7 @@
 cd "$(dirname $0)"; set -xe
 
 python3 -m pip uninstall automonisaur -y
-python3 -m pip install -e ./
+python3 -m pip install --use-pep517 -e ./
 
 if [ ! -z "$@" ]; then
   python3 -m pip install -U "$@"
