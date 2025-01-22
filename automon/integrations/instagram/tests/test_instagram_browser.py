af4fef0d839e7baa62a065722d558440e1a67013
naisanzaa <naisanza@gmail.com>
2025-01-22 05:36:31 -0600

    [instagram] add tests

--- automon/integrations/instagram/tests/test_instagram_browser.py
+++ automon/integrations/instagram/tests/test_instagram_browser.py
@@ -2,15 +2,22 @@ import unittest
 
 from automon.integrations.instagram.client_browser import InstagramBrowserClient
 
-c = InstagramBrowserClient(headless=True)
-c.start()
-
 
 class InstagramClientTest(unittest.TestCase):
+    c = InstagramBrowserClient(headless=False)
+    c.browser.run()
 
-    def test(self):
-        if c.is_ready():
-            c.browser.get(c.urls.login_page)
+    if c.is_running():
+        c.browser.get(c.urls.login_page)
+
+        # user
+        login_user = c.browser.wait_for_xpath(c.xpaths.login_user)
+        c.browser.action_click(login_user, 'user')
+        c.browser.action_type(c.login)
+
+        # password
+        password = c.browser.wait_for_xpath(c.xpaths.login_pass)
+        c.browser.action_click(password, 'password')
 
         c.browser.quit()
 
