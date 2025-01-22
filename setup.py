72914c9cf2c164726d9eacef748f4ea84fd5d321
naisanzaa <naisanza@gmail.com>
2025-01-22 09:44:33 -0600

    0.6.7
    Change log:
    
    [selenium] fix tests
    [selenium] fix don't raise exception when chrome driver is not set
    [selenium] add unit tests
    [selenium] fix minor expected function returns
    [selenium] add property current_window_handle
    [selenium] fix get_screenshot_as_file incorrect args
    [selenium] fix action_scroll_to_bottom
    [selenium] add browser tests
    [selenium] add proxy tests
    
    [facebook] fix proxy weights update
    [facebook] update proxy weight handling
    
    [instagram] fix missing browser
    [instagram] add tests
    
    [airport] remove async
    [airport] fix summary
    
    [helpers] fix env var not stripping single and double quotes
    [pypi] fix license

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.6.6",
+    version="0.6.7",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
