8d8a93400f97230af88223a0680565998dfad938
naisanzaa <naisanza@gmail.com>
2025-02-18 03:09:41 -0600

    [google auth] fixed leftover async removal

--- automon/integrations/google/auth/tests/test_config_Credentials.py
+++ automon/integrations/google/auth/tests/test_config_Credentials.py
@@ -1,14 +1,13 @@
 import unittest
-import asyncio
 
 from automon.integrations.google.auth import GoogleAuthConfig
 
 
 class MyTestCase(unittest.TestCase):
-    async def test_something(self):
+    def test_something(self):
         test = GoogleAuthConfig()
-        if await test.Credentials():
-            self.assertTrue(asyncio.run(test.Credentials()))
+        if test.Credentials():
+            self.assertTrue(test.Credentials())
 
 
 if __name__ == '__main__':
--- automon/integrations/google/auth/tests/test_google_auth.py
+++ automon/integrations/google/auth/tests/test_google_auth.py
@@ -1,16 +1,15 @@
 import unittest
-import asyncio
 
 from automon.integrations.google.auth import GoogleAuthClient
 
 
 class MyTestCase(unittest.TestCase):
-    async def test_authenticate(self):
+    def test_authenticate(self):
         test = GoogleAuthClient()
         # scopes = ['https://www.googleapis.com/auth/contacts.readonly']
         # client = AuthClient(serviceName='people', scopes=scopes)
         if test.authenticate():
-            self.assertTrue(asyncio.run(test.authenticate()))
+            self.assertTrue(test.authenticate())
 
 
 if __name__ == '__main__':
