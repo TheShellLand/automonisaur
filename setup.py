6b451f24f37394b7b17b692f7ed22c4a5653cdb2
naisanzaa <naisanza@gmail.com>
2025-02-20 07:37:49 -0600

    0.6.13
    Change log:
    
    [ollama] updated minor small things
    [ollama] fixed stream printing each chunk from llm
    [ollama] fixed clearer logging message for token usage
    [ollama] added starting ollama server
    [ollama] added a very rudimentary prompt chaining
    [ollama] fixed total tokens and max tokens shown in logs
    [ollama] fixed readme link
    
    [facebook] fixed proxy searching to just add proxy to the browser instead of trying to find a working one
    [facebook] fixed PROXY property with a `@property.setter`
    [facebook] fixed proxy searching to just add proxy to the browser instead of trying to find a working one
    
    [logging] fixed all imports to the new logging module location
    [logging] added import to helpers module
    [logging] fixed class object inheritance and super init, but function name and line number are still not working
    [logging] fixed circular imports
    
    [datadog] fixed ddtrace instrumentation causing pytest ``--log-cli-level DEBUG` to fail
    [automon] added integrations import

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.6.12",
+    version="0.6.13",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
