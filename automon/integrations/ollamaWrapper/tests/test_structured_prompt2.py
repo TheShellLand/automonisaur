b4d97e9ae8c6daa270a49b396b90dad8f268bd5a
naisanzaa <naisanza@gmail.com>
2025-02-23 02:54:59 -0600

    [ollama] added more tests

---
+++ automon/integrations/ollamaWrapper/tests/test_structured_prompt2.py
@@ -0,0 +1,59 @@
+import unittest
+import json
+
+from automon.integrations.ollamaWrapper import OllamaClient
+from automon.helpers.osWrapper import environ
+
+from automon.helpers.loggingWrapper import LoggingClient, DEBUG, ERROR
+
+LoggingClient.logging.getLogger('httpx').setLevel(ERROR)
+LoggingClient.logging.getLogger('httpcore').setLevel(ERROR)
+LoggingClient.logging.getLogger('automon.integrations.ollamaWrapper.client').setLevel(DEBUG)
+LoggingClient.logging.getLogger('automon.integrations.ollamaWrapper.utils').setLevel(ERROR)
+LoggingClient.logging.getLogger('automon.integrations.ollamaWrapper.chat').setLevel(ERROR)
+LoggingClient.logging.getLogger('opentelemetry.instrumentation.instrumentor').setLevel(ERROR)
+
+
+class TestOllamaClient(unittest.TestCase):
+    def test_chat(self):
+        model = OllamaClient()
+        model.STREAM = True
+
+        if not model.is_ready() or not environ('RUN'):
+            return
+
+        RAG = open('RAG.txt', 'r').read()
+
+        model.add_chain(
+            f"""
+            You are a chatbot agent answering customer's questions in a chat.
+            Your task is to answer questions using data provided in the <DATA> section.
+            
+            <DATA>
+            <RAW>
+            {RAG}
+            </RAW>
+            </DATA>
+            
+            <INSTRUCTIONS>
+            -   Always give a truthful and honest percentage of relevance of the resume to the job.
+            -   You are allowed to ask a follow up question if it will help clarify.
+            -   For everything else, please explicitly mention these notes. 
+            -   Answer in plain English and no sources are required.
+            -   Chat with the customer so far is under the CHAT section.
+            </INSTRUCTIONS>
+            
+            
+            QUESTION: Parse out all the urls. 
+            ANSWER:
+            
+            """
+        ).chat().add_chain(
+            f"""
+            QUESTION: Give me the list in a python list format.
+            """
+        ).chat()
+
+
+if __name__ == '__main__':
+    unittest.main()
