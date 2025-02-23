69e3d1101c4f4b32352149741ee7e7a5bfc38f88
naisanzaa <naisanza@gmail.com>
2025-02-22 21:18:47 -0600

    [tempfile] added human readable methods

--- automon/helpers/tempfileWrapper/__init__.py
+++ automon/helpers/tempfileWrapper/__init__.py
@@ -2,9 +2,23 @@ import tempfile
 
 
 class Tempfile(object):
+
+    @classmethod
+    def get_temp_dir(cls):
+        return cls.gettempdir()
+
     @staticmethod
     def gettempdir():
         return tempfile.gettempdir()
+
+    @classmethod
+    def make_temp_dir(cls):
+        return cls.mkdtemp()
+        empdir()
+
+    @classmethod
+    def make_temp_file(cls):
+        return cls.mkstemp()
 
     @staticmethod
     def mkdtemp():
