249ffc189d7ff176cb580333cf45020967e983a5
naisanzaa <naisanza@gmail.com>
2025-03-01 10:04:50 -0600

    [requests] fixed silent errors when exceptions are not raised

--- automon/integrations/requestsWrapper/tests/test_rest.py
+++ automon/integrations/requestsWrapper/tests/test_rest.py
@@ -9,7 +9,7 @@ class Client(unittest.TestCase):
     def test_get(self):
         self.assertTrue(r.get('https://1.1.1.1'))
         self.assertTrue(r.requests.get('https://1.1.1.1'))
-        self.assertFalse(r.get('x://127.0.0.1'))
+        self.assertRaises(Exception, r.get, ('x://127.0.0.1'))
 
 
 if __name__ == '__main__':
