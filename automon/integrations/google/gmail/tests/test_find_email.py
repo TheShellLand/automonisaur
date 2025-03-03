882bc012736cbc714212cfaff7865961a2914fba
naisanzaa <naisanza@gmail.com>
2025-03-03 10:43:08 -0600

    [google gmail] added tests

---
+++ automon/integrations/google/gmail/tests/test_find_email.py
@@ -0,0 +1,38 @@
+import unittest
+
+from automon.integrations.google.gmail import GoogleGmailClient, GoogleGmailConfig, Format
+from automon import LoggingClient, ERROR, DEBUG
+
+LoggingClient.logging.getLogger('httpx').setLevel(ERROR)
+LoggingClient.logging.getLogger('httpcore').setLevel(ERROR)
+LoggingClient.logging.getLogger('automon.integrations.ollamaWrapper.client').setLevel(DEBUG)
+LoggingClient.logging.getLogger('automon.integrations.ollamaWrapper.utils').setLevel(ERROR)
+LoggingClient.logging.getLogger('automon.integrations.ollamaWrapper.chat').setLevel(ERROR)
+LoggingClient.logging.getLogger('opentelemetry.instrumentation.instrumentor').setLevel(ERROR)
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        gmail = GoogleGmailClient()
+        # client.config.add_gmail_scopes()
+        gmail.config.add_scopes(
+            ["https://mail.google.com/"]
+        )
+
+        if gmail.is_ready():
+            gmail.config.Credentials()
+            gmail.config.userinfo()
+            gmail.config.build_service()
+            gmail.labels_list()
+            gmail.draft_list()
+            d = gmail.draft_list_automon()
+            gmail.users_getProfile()
+            # client.history_list(startHistoryId='73211814')
+            gmail.messages_list()
+            msg = gmail.messages_get('1953dbd795081667', format=Format.full)
+
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
