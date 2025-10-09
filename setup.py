7dbd7966d878a1bd4b6799ca67f7376ad2e65d4a
naisanza <naisanza@gmail.com>
2025-10-09 04:26:27 -0400

    0.6.20
    Change log:
    
    [gmail] updated Thread class to init with properties so it can still be referenced if it is not updated
    [gmail] added max_retries
    [gmail] added `get_resume`
    [gmail] added more debugging
    [gmail] updated Message to use more properties
    [gmail] updated
    [gmail] refactored into AutomonGmailClient
    [gmail] fix Message class to handle assertions
    [gmail] minor update to logging
    [gmail] update thread label instead of message
    [gmail] support searching thread by subject
    [gmail] fix resume
    [gmail] minor logging fix
    [gmail] fix missing messages
    [gmail] add chat thru email
    [gmail] fix creating automon labels
    [gmail] refactor deleting extra message data to make the prompt smaller
    [gmail] support for chatting with the model
    [gmail] support for getting the full thread
    [gmail] handle threads instead of messages
    [gmail] reorder draft cleanup
    [gmail] fix clean thread
    [gmail] add debug logging
    [gmail] fix method automon_clean_thread to not disregard TRASH items
    [gmail] fix function needs_followup to use the thread
    [gmail] fix messages_attachments_get attachment size
    [gmail] remove typing library for better backwards compatibility
    [gmail] fix Message repr
    [gmail] fix analyze email thread instead of just the resume
    [gmail] add processing marker, fix analysis
    [gmail] add processing marker
    [gmail] fix resume selection
    [gmail] fix self clean thread
    [gmail] add error label
    [gmail] add label for marking the active current message
    [gmail] refactoring
    [gmail] add first and latest clean threads
    [gmail] add debug label
    [gmail] update follow up logic
    [gmail] no free tier in gemini 2.5 pro preview
    [gmail] no free tier in gemini 2.5 pro preview
    [gmail] update random model seletcion
    [gmail] add automon_clean_thread
    [gmail] retry with a different gemini model
    [gmail] try new gemini models
    [gmail] fixed time delta check [gmail] always delete drafts
    [gmail] refactor time delta
    [gmail] refactor auto reply logic
    [gmail] fixed analyze
    [gmail] updated label logging
    [gmail] fixed error retrieving message attachments
    [gmail] fixed analyze
    [gmail] updated label logging
    [gmail] fixed error retrieving message attachments
    [gmail] fixed attachment handling
    [gmail] fixed email attachment boolean
    
    [gemini] refactored
    [gemini] update docstring
    [gemini] update model usage counter
    [gemini] update model docstrings
    [gemini] add new models
    [gemini] fixed followup prompt
    [gemini] added downloading from the web
    
    [google oauth] fix credentials_file_client_id
    [google oauth] fix minor logging
    [google oauth] fix getting client id from credentials_file_client_id
    
    [minio] fixed handling of Bucket
    [minio] fixed error when getting a bucket that does not exist
    [minio] fixed when deleting a bucket that doesn't exist
    [minio] fixed when bucket doesn't exist it will return an empty Bucket object
    
    [helpers] added a debugger to just print
    [helpers] add DictUpdate
    
    [xsoar] update classes
    [xsoar] update client, tests
    [xsoar] add create, search, delete incident
    [xsoar] update client and config
    [xsoar] add create incident
    [xsoar] update client
    
    [panorama] update client and config
    [panorama] add client
    
    [requests] fix methods to return expected type
    [requests] fix data type
    
    [json] added dumping a dict into a json string but with all dict values turned into a string
    
    [pip] install from gihub repo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="automonisaur",
-    version="0.6.19",
+    version="0.6.20",
     author="naisanza",
     author_email="naisanza@gmail.com",
     description="Core libraries for automonisaur",
