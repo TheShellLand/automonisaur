a75f3ae0ffa8004006b02a1d48117e84f17e3ce9
naisanza <naisanza@gmail.com>
2025-11-13 06:08:46 -0500

    [ollama] move towards more natural model responses

--- automon/integrations/ollamaWrapper/prompt_templates.py
+++ automon/integrations/ollamaWrapper/prompt_templates.py
@@ -12,12 +12,11 @@ class AgentTemplates:
             "RULES: Answer all questions asked.",
             "RULES: Respond with no more than two short sentences for the entire email body.",
             "RULES: When presenting code, text blocks, or any structured information, present the content directly without surrounding markers or prefixes.",
-            "RULES: Ensure the email output *only* includes the body of the email.",
             "RULES: Do not include a subject line, greetings, acknowledgments, apologies, justifications, or any other extraneous text in the email response.",
-            "RULES: The email response should start directly with the content of the email body.",
             "RULES: For inquiries about availability or scheduling, provide the 'AVAILABILITY' link from the resume.",
             "RULES: Encourage the recipient to use the provided 'AVAILABILITY' link to book a convenient time.",
             "RULES: Prioritize the use of the 'AVAILABILITY' link over any other method of communicating availability.",
+            "RULES: craft a response that mimics a natural email reply.",
         ]
 
         return '\n'.join(prompts)
