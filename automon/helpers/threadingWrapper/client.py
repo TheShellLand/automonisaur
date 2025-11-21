e0c6478f2a8901e684f2965a41714ceb8f51909d
naisanza <naisanza@gmail.com>
2025-11-21 03:12:48 -0500

    [threading] decoupled thread list from class attribute
    this ensures threads are protected, and not mixed into other instances

--- automon/helpers/threadingWrapper/client.py
+++ automon/helpers/threadingWrapper/client.py
@@ -12,6 +12,7 @@ class Thread(threading.Thread):
     def __init__(self, group=None, target=None, name=None, args=(), kwargs={}):
         super().__init__(group, target, name, args, kwargs)
         # Store arguments as public attributes
+        self.public_target = target
         self.public_args = args
         self.public_kwargs = kwargs
 
@@ -19,14 +20,33 @@ class Thread(threading.Thread):
 
 
 class ThreadingClient(object):
-    global_threads_max: int = 10
-    global_threads: list = []
+    _global_threads_max: int = 1
+    _global_threads_max_lock: threading.Lock = threading.Lock()
 
     def __init__(self):
         self.worker_queue: queue.Queue = queue.Queue()
         self.completed_queue: queue.Queue = queue.Queue()
+        self.threads_list: list[Thread] = []
 
         self.exit_event = threading.Event()
+        self.retry: bool = False
+
+    def _thread_wrapper(self, target, args):
+        current_thread = threading.current_thread()
+
+        while True:
+            try:
+                log.debug(f"[ThreadingClient] :: {current_thread.name} :: wrapper started")
+                result = target(*args)
+                current_thread.result = result
+                self.completed_queue.put(result)
+                break
+            except Exception as error:
+                log.error(f"[ThreadingClient] :: ERROR :: {error=}")
+                current_thread.exception = error
+
+            if not self.retry:
+                break
 
     def add_worker(self, target: object, args: tuple):
         assert type(args) is tuple
@@ -35,39 +55,75 @@ class ThreadingClient(object):
         log.debug(f'[ThreadingClient] :: add_worker :: {target=} :: {args=}')
         return self
 
-    def increase_global_threads_max(self):
-        """doubles every worker completed"""
-        old_value = self.global_threads_max
-        new_value = int(self.global_threads_max * 2)
-
-        self.global_threads_max = new_value
-
-        log.debug(f'[ThreadingClient] :: global_threads_max :: {old_value} -> {new_value}')
-        return self
-
     def decrease_global_threads_max(self):
         """drops to 25%"""
-        old_value = self.global_threads_max
-        new_value = int(self.global_threads_max * 0.25)
+        with ThreadingClient._global_threads_max_lock:
+            old_value = ThreadingClient._global_threads_max
+            new_value = int(ThreadingClient._global_threads_max * 0.25)
 
-        self.global_threads_max = new_value
+            ThreadingClient._global_threads_max = new_value
 
         log.debug(f'[ThreadingClient] :: global_threads_max :: {old_value} -> {new_value}')
         return self
 
-    def start(self):
+    def increase_global_threads_max(self):
+        """doubles every worker completed"""
+        with ThreadingClient._global_threads_max_lock:
+            old_value = ThreadingClient._global_threads_max
+            new_value = int(ThreadingClient._global_threads_max * 2)
 
-        while self.worker_queue.qsize() > 0 or any(t.is_alive() for t in self.global_threads):
+            ThreadingClient._global_threads_max = new_value
 
-            self.global_threads = [t for t in self.global_threads if t.is_alive()]
+        log.debug(f'[ThreadingClient] :: global_threads_max :: {old_value} -> {new_value}')
+        return self
 
-            if self.worker_queue.qsize() > 0 and self.total_global_threads < self.global_threads_max:
+    def is_done(self):
+        # Returns True if ALL threads are not alive (i.e., finished)
+        self.threads_list = [t for t in self.threads_list if t.is_alive()]
+
+        all_threads_terminated = all(not t.is_alive() for t in self.threads_list)
+
+        # The client is 'done' only when BOTH conditions are met
+        return self.worker_queue.qsize() == 0 and all_threads_terminated
+
+    def results(self):
+        while self.completed_queue.qsize() > 0:
+            result = self.completed_queue.get()
+            if result.exception:
+                log.warning(f"[ThreadingClient] :: ERROR :: {result.exception=}")
+            log.debug(f'[ThreadingClient] :: results :: {result}')
+            yield result
+
+    def start(self, max_threads: int = None, retry: bool = False):
+
+        if max_threads:
+            with ThreadingClient._global_threads_max_lock:
+                ThreadingClient._global_threads_max = max_threads
+
+        if retry:
+            self.retry = retry
+
+        while not self.is_done():
+
+            self.threads_list = [t for t in self.threads_list if t.is_alive()]
+            current_threads_count = len(self.threads_list)
+
+            with ThreadingClient._global_threads_max_lock:
+                max_threads_limit = ThreadingClient._global_threads_max
+
+            if self.worker_queue.qsize() > 0 and current_threads_count < max_threads_limit:
+
                 function, args = self.worker_queue.get()
-                thread = Thread(target=function, args=tuple(args))
+
+                # thread = Thread(target=function, args=args)
+                thread = Thread(target=self._thread_wrapper,
+                                args=(function, args))
+
+                self.threads_list.append(thread)
+
                 thread.start()
-                self.global_threads.append(thread)
                 log.debug(f'[ThreadingClient] :: start :: {thread.name} :: running: '
-                          f'{len(self.global_threads)} threads ({self.global_threads_max} max)')
+                          f'{current_threads_count + 1} threads ({max_threads_limit} max)')
 
             else:
                 time.sleep(0.1)
@@ -75,28 +131,20 @@ class ThreadingClient(object):
             if self.exit_event.is_set():
                 break
 
-        for thread in self.global_threads:
+        for thread in self.threads_list:
             thread.join()
-            self.completed_queue.put(thread)
 
-        log.debug(f'[ThreadingClient] :: start :: {self.completed_queue.qsize()} threads completed')
+        log.debug(f'[ThreadingClient] :: start :: {self._completed_threads_count} threads completed')
         return self
 
-    def results(self):
-        while self.completed_queue.qsize() > 0:
-            result = self.completed_queue.get()
-            log.debug(f'[ThreadingClient] :: results :: {result}')
-            yield result
+    @property
+    def _current_threads_count(self):
+        return len(self.threads_list)
 
-    def is_done(self):
-        """Non-blocking check to see if all threads are done."""
-        # Returns True if ALL threads are not alive (i.e., finished)
-        return all(not t.is_alive() for t in self.running_threads)
+    @property
+    def _completed_threads_count(self):
+        return self.completed_queue.qsize()
 
     def stop(self):
         self.exit_event.set()
         return self
-
-    @property
-    def total_global_threads(self):
-        return len(self.global_threads)
