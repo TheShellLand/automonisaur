938d1a9fcce31e2ee7cec419c64e54d29ba61b08
naisanza <naisanza@gmail.com>
2026-05-28 18:48:35 -0700

    [helpers] helpers to check if text contains true or false

--- automon/helpers/__init__.py
+++ automon/helpers/__init__.py
@@ -3,13 +3,14 @@ from .debug import debug, debug_exception, debug_str
 from .dictWrapper import DictHelper
 from .encapsulation import encapsulate
 from .loggingWrapper import LoggingClient, INFO, DEBUG, ERROR, CRITICAL
-from .markdown import Chat, Format, list_to_markdown, lstrip_str
+from .markdown import Chat, Format, Markdown
 from .networking import Networking
 from .regex import Regex
 from .repr import repr_str
 from .sleeper import Sleeper
 from .subprocessWrapper import Run
 from .tempfileWrapper import Tempfile
+from .true_or_false import is_true, is_false
 from .queueWrapper import UniqueQueue
 
 import automon.helpers.loggingWrapper
---
+++ automon/helpers/true_or_false.py
@@ -0,0 +1,19 @@
+from automon import debug_exception
+
+
+def is_true(text: str) -> bool | None:
+    if 'true' in str(text).lower():
+        return True
+    if 'false' in str(text).lower():
+        return False
+    # raise Exception(f"[GoogleGeminiClient] :: response_is_true :: neither true or false")
+    raise debug_exception(locals(), 'neither true or false')
+
+
+def is_false(text: str) -> bool | None:
+    if 'false' in str(text).lower():
+        return True
+    if 'true' in str(text).lower():
+        return False
+    # raise Exception(f"[GoogleGeminiClient] :: response_is_false :: neither true or false")
+    raise debug_exception(locals(), 'neither true or false')
