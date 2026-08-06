9082d1d05894489cfa238be98fbfaeb26631b5f7
naisanza <naisanza@gmail.com>
2026-08-06 10:01:29 +0000

    [Google] Comment out maxResults in test_client_autoreply

--- automon/integrations/google/gmail/tests/test_client_autoreply.py
+++ automon/integrations/google/gmail/tests/test_client_autoreply.py
@@ -169,7 +169,7 @@ def get_threads(gmail: AutomonGmailClient):
 
             for query in query_sequence:
                 thread_search = gmail.thread_list_automon(
-                    maxResults=3,
+                    # maxResults=3,
                     pageToken=nextPageToken,
                     labelIds=query,
                 )
